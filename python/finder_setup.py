c6d403292dd62163c033bcd1d7c7432f7c3a2128
burnett <>
2007-07-19 14:07:57 +0000

    *** empty log message ***

--- python/finder_setup.py
+++ python/finder_setup.py
@@ -1,25 +1,13 @@
 #  setup for pointlike source finder
-# $Header$
-
+# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/finder_setup.py,v 1.1 2007/07/19 13:52:13 burnett Exp $
+print 'setup'
 # data selection parameters
 
-radius = 7.0   # radius in degrees for initial data selection
-event_type = -1 # 0, select front only; -1 no selection
+radius = 180   # radius in degrees for initial data selection
+event_class = -1 # 0, select front only; -1 no selection
                # note that current algorithm only works on front-conversion events
 source_id =-1  # -1: all sources -- select according to Monte Carlo source id, if present
 
-
-# HEALpix level range for energy band  fits
-
-minlevel=8   # minimum level to use for fits (>-6)  
-maxlevel=13  # maximum level for fits  (<=13)
-
-# parameters governing iteration cycle of bands/position
-
-TSmin= 5     # minimum TS value to allow during iteration
-skip1=1      # inital number of layers to skip in localization fit
-skip2=4      # don't skip beyond this
-itermax=2    # maximum number of iterations
 
 verbose = 0  # set non-zero to get lots of output
 
@@ -29,20 +17,19 @@ path = "F:/glast/data/SourceDetection"  #location on glast-ts
 files = [path+"/pl_0_events_0000.fits" ,path+"/bg_low_0_events_0000.fits"]
 # parameters.
 
-eventType=-1
-center   = SkyDir(0,0, SkyDir.GALACTIC) 
-radius   = 180 # for all sky
+# direction and cone or radius about it to examine
+ra=0
+dec=0
+radius   = 180 # 180 for all sky
 
+pix_level = 8  # pixel level to search
+count_threshold=20 # 
+
+TSmin   = 10   # minimum TS for candidates
+
+# parameter for pruning: radius in degrees
 prune_radius = 0.25
 
-TSmin   = 10
-eq_TS_min =15 #25.,
-mid_TS_min= 15 #19.,
-polar_TS_min= 15 #18.,
-pix_level = 8
-count_threshold=20 # was 12
-skip_TS = 2
-
 # file to write results to
-tablefile='sources.txt'
+outfile='sources.txt'
         
