55c3ab9c8a5bfbbd6b4d89d0c13d3c02a6ee01ed
burnett <>
2007-08-12 04:19:02 +0000

    create pointfind application; use python module for data

--- pointlike/Data.h
+++ pointlike/Data.h
@@ -1,7 +1,7 @@
 /** @file Data.h 
     @brief declaration of the Data wrapper class
 
-    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/pointlike/Data.h,v 1.2 2007/06/25 20:59:25 burnett Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/pointlike/Data.h,v 1.3 2007/07/14 03:50:54 burnett Exp $
 */
 
 
@@ -13,6 +13,7 @@ class PhotonMap;
 }
 #include <string>
 #include <vector>
+#include "embed_python/Module.h"
 
 namespace pointlike {
 /***
@@ -20,6 +21,14 @@ wrapper for PhotonMap-- maybe move there
 */
 class Data {
 public:
+    /** constructor uses python module, look for following attributes:
+        pixelfile  if set, use it to find table PHOTONMAP, otherwise, expect files
+        files      list of files, either FITS or ROOT
+        event_class [-1]
+        source_id [-1]
+
+    **/
+    Data( embed_python::Module& setup);
 
     //! constructor loads data from a fits FT1 or root file (MeritTuple) to make a PhotonMap
     //! @param event_type 0 for class A front, etc, -1 for all
@@ -29,16 +38,12 @@ public:
     //! constructor loads data from a list of fits or root files to make a PhotonMap
     //! @param event_type 0 for class A front, etc, -1 for all
     //! @param source_id select given source
-    Data(std::vector<std::string> files, int event_type, int source_id=-1, 
-        std::string ft2file=""
-        );
-#if 0 // not currently implemented?
+    Data(std::vector<std::string> files, int event_type, int source_id=-1 );
+
     //! constructor loads a PhotonMap that was saved in a fits file
     //! @param inputFile the fits file name
     //! @param tablename ["PHOTONMAP"] the fits table name
     Data(const std::string & inputFile, const std::string & tablename="PHOTONMAP");
-#endif
-
 
     //! add  data from the file to current set
     //! @param file Either FT1 or  MeritTuple ROOT file
@@ -52,12 +57,13 @@ public:
     //! same as above, for python use
     const map_tools::PhotonMap& map()const{return *m_data;}
 
+    ~Data();
+
     static double s_scale[4]; // scale factors
     static double set_scale(int i, double s){double t(s_scale[i]);  s_scale[i]=s; return t;}
 
 private:
     map_tools::PhotonMap * m_data;
-    std::string m_ft2file;
 };
 
 }
