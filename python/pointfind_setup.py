e48975190409313aac992bd1d35c5f083334f946
burnett <>
2008-05-28 21:41:52 +0000

    current setup for testing

--- python/pointfind_setup.py
+++ python/pointfind_setup.py
@@ -1,5 +1,5 @@
 #  setup for pointlike source finder
-# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointfind_setup.py,v 1.10 2008/01/25 22:35:14 burnett Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointfind_setup.py,v 1.11 2008/04/01 13:28:07 burnett Exp $
 
 from pointlike_defaults import *
 
@@ -12,7 +12,7 @@ Data.pixelfile = r'F:\glast\data\SC2\obssim\allsky_noGRBs.fits'
 
 # choose region to search
 SourceFinder.ra, SourceFinder.dec = 248.79, -17.86 # 3EGJ1635m1751 
-SourceFinder.ra, SourceFinder.dec = 137.5, 59.8 # DC2_3EGJ1048m5840 
+#SourceFinder.ra, SourceFinder.dec = 137.5, 59.8 # DC2_3EGJ1048m5840 
 SourceFinder.examine_radius   =  5# 180 for all sky
 
 #Data.files=[r'F:\glast\data\SC2\obssim\month_test2a\merged_files_v1.fits']
@@ -21,11 +21,12 @@ SourceFinder.examine_radius   =  5# 180 for all sky
 #Data.pixelfile='test2a_photonmap.fits'
 
 # file to write a table of results to
-#SourceFinder.outfile='../output/J1048m5840b.txt'
-#print 'will write to file %s '% SourceFinder.outfile
+SourceFinder.outfile='../output/J1048m5840b.txt'
+SourceFinder.outfile='../output/3EGJ1635m1751.txt'
+print 'will write to file %s '% SourceFinder.outfile
 
 
 SourceFinder.refit = 1  # turn on refit
-SourceFinder.regfile = '../output/refit.reg'
+SourceFinder.regfile = '../output/3EGJ1635m1751.reg'
 #SourceFinder.fitsfile = '../output/refit.fits'
-PointSourceLikelihood.minlevel=6
+#PointSourceLikelihood.minlevel=6
--- python/pointlike_defaults.py
+++ python/pointlike_defaults.py
@@ -1,6 +1,6 @@
 # default parameters for the various parameter files
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointlike_defaults.py,v 1.5 2008/01/29 19:17:52 burnett Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointlike_defaults.py,v 1.6 2008/02/19 21:00:33 burnett Exp $
 #
 # Include this to set defaults, then override 
 import sys
@@ -30,8 +30,7 @@ class Diffuse:
 class PointSourceLikelihood: #parameters for the likelihood calculation
     # HEALpix level range for energy band  fits
 
-    minlevel=8   # minimum level to use for fits (>-6)  
-    maxlevel=13  # maximum level for fits  (<=13)
+    emin = 700;   # only select bands including or above this energy
     minalpha=0.15# minimum value for signal fraction to use in TS total
 
     # parameters governing iteration cycle of bands/position
@@ -53,10 +52,10 @@ class PointSourceLikelihood: #parameters for the likelihood calculation
 class SourceFinder:  # parameters for the SourceFinder.
 
     TSmin = 8.0          # overall minimum TS
-    pixLevel = 8         # pixelization level for first pass.  Higher number -> more points tested for possible sources
-    pixel_fraction = 0.2 # fraction of pixels to sample, sorted according to weighted photon count
-    group_radius = 1.0   # radius for making groups
+    pixel_fraction = 1.0 # fraction of pixels to sample, sorted according to weighted photon count
     prune_radius =0.25   # pruning radius in degrees (also needs to be tuned)
+    group_radius = 4.0   # maximum radius for nearby sources.
     examine_radius=180   # default is to examine full sky
+    pass1_nside=256      #
             
 
