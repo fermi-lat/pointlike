3e6d283290ab57c2af63585bb19540db9234d9d2
burnett <>
2008-06-29 01:53:48 +0000

    ready to tag

--- python/pointfind_setup.py
+++ python/pointfind_setup.py
@@ -1,5 +1,5 @@
 #  setup for pointlike source finder
-# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointfind_setup.py,v 1.12 2008/05/28 21:41:52 burnett Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointfind_setup.py,v 1.13 2008/06/24 19:06:02 burnett Exp $
 
 from pointlike_defaults import *
 
@@ -8,6 +8,7 @@ from pointlike_defaults import *
 
 #specify data: pixels or a list of FT1 files
 Data.pixelfile = r'F:\glast\data\SC2\obssim\allsky_noGRBs.fits'
+Data.pixelfile = r'F:\glast\data\obssim3_v2\uw\pixels_all_days.fits'
 
 # choose region to search
 SourceFinder.l,SourceFinder.b= 0,0
@@ -15,10 +16,11 @@ SourceFinder.examine_radius = 180 # 180 for all sky
 
 
 # files to write a table of results to
-path = '../output/obssim2/'
-SourceFinder.outfile=  path+'pointfind_f.txt'
-SourceFinder.regfile = path+'pointfind_f.reg'
-SourceFinder.logfile = path+'pointfindlog_f.txt' # the log file
+path = '../output/obssim3/'
+suffix='a'
+SourceFinder.outfile=  path+'pointfind_'+suffix+'.txt'
+SourceFinder.regfile = path+'pointfind_'+suffix+'.reg'
+SourceFinder.logfile = path+'pointfindlog_'+suffix+'.txt' # the log file
 
 print 'will write to file %s '% SourceFinder.outfile
 SourceFinder.group_radius = 2.0
