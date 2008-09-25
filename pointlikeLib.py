b5d047783144f84c0fbe2dd849f45c52e5501ddb
glastrm <>
2008-09-25 02:59:10 +0000

    added minuit as a dependency

--- pointlikeLib.py
+++ pointlikeLib.py
@@ -1,12 +1,13 @@
 # @file pointlikeLib.py
 # @brief scons package dependencies
 #
-#$Id: pointlikeLib.py,v 1.3 2008/02/24 19:06:34 burnett Exp $
+#$Id: pointlikeLib.py,v 1.4 2008/02/26 05:25:20 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['pointlike'])
     depends = 'astro healpix skymaps embed_python'.split()
     for pack in depends: env.Tool(pack+'Lib')
+    env.Tool('addLibrary', library=env['minuitLibs'])
     
 def exists(env):
     return 1
