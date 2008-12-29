b93b28095757629dd9d67d07ced6b84ceddcf3cf
burnett <>
2008-12-29 04:16:32 +0000

    clean up a bit: follow BDFL style for includes

--- python/pixeldata.py
+++ python/pixeldata.py
@@ -2,11 +2,16 @@
 Manage data and livetime information for an analysis
 
 
-    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pixeldata.py,v 1.2 2008/11/28 20:43:34 burnett Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pixeldata.py,v 1.3 2008/12/15 22:12:26 burnett Exp $
 
 """
-version='$Revision: 1.2 $'.split()[1]
+version='$Revision: 1.3 $'.split()[1]
 import os
+import psf
+import math
+import skymaps
+import pointlike
+
 
 class PixelData(object):
 
@@ -21,7 +26,6 @@ Create a new PixelData instance, managing data and livetime.
 
     event_files: a list of event files (FT1) to process (or a single file). 
     history_files: a list of spacecraft history files (FT2) to process (or a single file).
-    diffusefile:  Full path to a galactic diffuse file, like  GP_gamma_conventional.fits  (note isotropic)                                 
 
 Optional keyword arguments:
 
@@ -35,8 +39,8 @@ Optional keyword arguments:
   align       [False] if True, perform boresigh alignment correction on pre-September flight data
   
   datafile    [None]  HEALpix data file: if specified and not found, create from FT1 info                                                                               
-  class_level [ 3]  select class level (set 0 for gtobssim                                                             
-  binsperdecade [4] When generating Bands from the FT1 data.
+  class_level [ 3]  select class level (set 0 for gtobssim )                                                            
+  binsperdecade [4] When generating Bands from the FT1 data. 
   use_mc_psf  [False] Use PSF determined by MC analysis of true; otherwise as defined by data
   quiet       [False] Set True to suppress (some) output 
   verbose     [False] More output
@@ -62,8 +66,8 @@ Optional keyword arguments:
                 self.__dict__[key] = value
 	    
         # check explicit files
-        for filename in  [self.event_files[0], self.history_files[0]] :
-            if filename is not None and not os.path.exists(filename):
+        for filelist in  [self.event_files, self.history_files] :
+            if filelist is not None and len(filelist)>0 and not os.path.exists(filelist[0]):
                 raise Exception('PixelData setup: file name or path "%s" not found'%filename)
 
         self.lt =  self.get_livetime()
@@ -72,29 +76,25 @@ Optional keyword arguments:
 
     
     def get_data(self):
-        from skymaps import PhotonBinner
-        from pointlike import Data
 
-        Data.set_class_level(self.class_level)
-        Data.set_zenith_angle_cut(self.zenithcut)
+        pointlike.Data.set_class_level(self.class_level)
+        pointlike.Data.set_zenith_angle_cut(self.zenithcut)
 
         if self.datafile is None or not os.path.exists(self.datafile):
-            self.binner = PhotonBinner(self.binsperdecade) # per decade
-            Data.setPhotonBinner(self.binner)
+            self.binner = skymaps.PhotonBinner(self.binsperdecade) # per decade
+            pointlike.Data.setPhotonBinner(self.binner)
             if self.verbose: print 'loading file(s) %s' % self.event_files
-            data = Data(self.event_files)
+            data = pointlike.Data(self.event_files)
             if self.verbose: print 'done'
             if self.datafile is not None:
                 if not self.quiet: print 'saving datafile %s for subsequent use' % self.datafile
                 data.write(self.datafile)
         else:
-            data = Data(self.datafile)
+            data = pointlike.Data(self.datafile)
             print 'loaded datafile %s ' % self.datafile
 
         # modify the psf parameters in the band objects, which SimpleLikelihood will then use
-        from psf import PSF
-        import math
-        self.psf = PSF(use_mc=self.use_mc_psf)
+        self.psf = psf.PSF(use_mc=self.use_mc_psf)
         
         if not self.quiet: print 'setting PSF parameters (use_mc=%d)'%self.use_mc_psf
         if self.verbose: print '  energy class  gamma sigma(deg)'
@@ -111,30 +111,35 @@ Optional keyword arguments:
         return data
 
     def get_livetime(self,   pixelsize=1.0):
-        from skymaps import LivetimeCube, Gti, SkyDir
-        from math import cos,radians
         if self.livetimefile is None or not os.path.exists(self.livetimefile):
             if self.roi_dir is None:
                 # no roi specified: use full sky
-                self.roi_dir = SkyDir(0,0)
+                self.roi_dir = skymaps.SkyDir(0,0)
                 self.roi_radius = 180
-            lt = LivetimeCube(
+            lt = skymaps.LivetimeCube(
                 cone_angle=self.roi_radius,\
                 dir=self.roi_dir,\
                 pixelsize=pixelsize,\
-                zcut=cos(radians(self.zenithcut)),\
+                zcut=math.cos(math.radians(self.zenithcut)),\
                 quiet=self.quiet )
             #Not right?  Needs fixin'.
             #g = Gti(self.event_files[0])
             #for n in xrange(1,len(self.event_files)):
-            #   g = g.intersect(Gti(self.event_files[n]))
+            #   g = g.intersect(skymaps.Gti(self.event_files[n]))
             #for hist in self.history_files:
             #   lt.load(hist,g)
+            
+            # if multiple history files, assume that there is one per event file, 
+            # and apply the Gti (unsafe!)
+            
             for n, hist in enumerate(self.history_files):
-                lt.load(hist,  Gti(self.event_files[n]))
+                lt.load(hist,  skymaps.Gti(self.event_files[n]))
+                
+            # write out ltcube if requested
             if self.livetimefile is not None: lt.write(self.livetimefile)
         else: 
-            lt = LivetimeCube(self.livetimefile)
+            # ltcube exists: just use it! (need to bullet-proof this)
+            lt = skymaps.LivetimeCube(self.livetimefile)
             print 'loaded LivetimeCube %s ' % self.livetimefile
         return lt
 
@@ -144,7 +149,6 @@ Optional keyword arguments:
 #--------------------------------------------------------------------
 
 def setup(bins=8):
-    import os
     data_path = r'f:\glast\data\flight'
     months = ['aug', 'sep', 'oct']
     event_files  = [os.path.join(data_path, '%s2008-ft1.fits'%m) for m in months ]
