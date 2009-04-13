e1790c2d29d4fc560ca9b87a700d51bde93a5021
burnett <>
2009-04-13 22:57:00 +0000

    logfile stuff; allow update of PSF parameters.

--- src/pointfit/pointfit_main.cxx
+++ src/pointfit/pointfit_main.cxx
@@ -1,7 +1,7 @@
 /** @file pointfit_main.cxx
     @brief  Main program for pointlike localization fits
 
-    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/src/pointfit/pointfit_main.cxx,v 1.31 2008/12/29 04:12:14 burnett Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/src/pointfit/pointfit_main.cxx,v 1.32 2009/03/08 01:36:23 burnett Exp $
 
 */
 #include "pointlike/SourceList.h"
@@ -10,6 +10,8 @@
 #include "pointlike/ParamOptimization.h"
 
 #include "embed_python/Module.h"
+#include "skymaps/IParams.h"
+#include "skymaps/BinnedPhotonData.h"
 
 #include <iostream>
 #include <iomanip>
@@ -24,6 +26,39 @@ void help(){
 
 }
 
+void fix_psf(pointlike::Data &data)
+{
+    using std::setw;
+    using std::setprecision;
+    using std::left;
+    using std::fixed;
+
+    int w(12);
+
+    skymaps::BinnedPhotonData& pdata  = data.map();
+    std::cout << " Setting new values for PSF parameters" << std::endl;
+    std::cout << setw(w)<<"emin" << setw(w) << "class" << setw(w) << "sigma" << setw(w) << "gamma" << std::endl;
+    for( skymaps::BinnedPhotonData::iterator it = pdata.begin(); it!=pdata.end(); ++it){
+        skymaps::Band& b = *it;
+        double ebar( sqrt(b.emin()*b.emax()) );
+        int eclass(b.event_class());
+        double 
+            sigma( skymaps::IParams::sigma(ebar, eclass) ),
+            gamma( skymaps::IParams::gamma(ebar, eclass) );
+        b.setSigma(sigma);
+        b.setGamma(gamma);
+
+        std::cout << setw(w) << "  "<<setprecision(0)<< fixed<< left<< b.emin() 
+            << setw(w) << eclass
+            << setprecision(5) 
+            << setw(w) << b.sigma() 
+            << setw(w) << setprecision(2) <<b.gamma() 
+            << std::endl; 
+               
+    }
+    std::cout << setprecision(5);
+
+}
 
 int main(int argc, char** argv)
 {
@@ -33,6 +68,8 @@ int main(int argc, char** argv)
 
     int rc(0);
     try{
+
+        skymaps::IParams::init();
 
         std::string python_path("../python");
 
@@ -47,6 +84,8 @@ int main(int argc, char** argv)
         // use the  Data class to create the PhotonData object
         Data healpixdata(setup);
 
+        fix_psf(healpixdata);
+
         // print out summary of the the data used?
         healpixdata.info(); 
 
@@ -58,6 +97,14 @@ int main(int argc, char** argv)
             out = new std::ofstream(outfile.c_str());
         }
         SourceList::set_data(& healpixdata.map());
+        std::string logfile;
+        setup.getValue("logfile", logfile, logfile);
+        std::ofstream* log(0);
+        if( !logfile.empty() ){
+            log = new std::ofstream(logfile.c_str());
+            SourceList::set_log(log);
+            std::cout << "logging to file " << logfile<< std::endl;
+        }
 
         SourceList* sl(0);
         std::map<std::string, std::vector<double> > sourcedict;
@@ -76,6 +123,7 @@ int main(int argc, char** argv)
         double tsmin(10);
         setup.getValue("tsmin", tsmin, tsmin);
 
+        sl->sort_TS(); // order for refit
         sl->refit(); 
         if(tsmin>0) sl->filter_TS(tsmin); // filter
         sl->sort_ra(); // now by ra
@@ -91,6 +139,8 @@ int main(int argc, char** argv)
         // try to call the finish function, if defined
         PyObject* finish(setup.attribute("finish", false) );
         if(finish!=0) setup.call(finish);
+
+        delete log; // close
         
     }catch(const std::exception& e){
         std::cerr << "Caught exception " << typeid(e).name() 
