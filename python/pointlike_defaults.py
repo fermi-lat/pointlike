95e060a5438ec0e633dcc74e839f88630045c905
burnett <>
2009-04-13 22:55:02 +0000

    change minalpha default to 0.01

--- python/pointlike_defaults.py
+++ python/pointlike_defaults.py
@@ -1,6 +1,6 @@
 # default parameters for the various parameter files
 #
-# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointlike_defaults.py,v 1.15 2009/02/17 18:06:33 burnett Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/pointlike_defaults.py,v 1.16 2009/02/24 20:50:52 burnett Exp $
 #
 # Include this to set defaults, then override 
 import sys
@@ -40,7 +40,9 @@ class PointSourceLikelihood: #parameters for the likelihood calculation
     # HEALpix level range for energy band  fits
 
     emin = 200;   # only select bands including or above this energy
-    minalpha=0.   # minimum value for signal fraction to use in TS total
+    minalpha=0.01 # minimum value for signal fraction to use in TS total
+    minROI  = 0 # minimum size for ROI (deg)
+    maxROI = 20   # prevent from bing too large
 
     # parameters governing iteration cycle of bands/position
 
@@ -53,15 +55,6 @@ class PointSourceLikelihood: #parameters for the likelihood calculation
     maxstep = 0.2 # max step allowed during localization: abort if larger
     merge=1     # set to zero to not attempt to merge bands with identical parameters
     
-class SourceLikelihood: #parameters for the likelihood calculation
-
-    TSmin= -100.   # minimum TS value to allow during iteration
-    emin = 200     # minimim energy for bands
-    verbose = 0    # set non-zero to get lots of output
-    maxstep = 0.2  # max step allowed during localization: abort if larger
-    umax=50.
-    bgROI=0.5
-
 
 class SourceFinder:  # parameters for the SourceFinder.
 
