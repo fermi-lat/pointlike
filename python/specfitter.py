cca84a7fe02fd31fe4022df28c52db7fa36454a4
kerrm <>
2009-09-17 21:22:42 +0000

    Preliminary work on making error estimation more robust -- but needs to be revisited.

--- python/specfitter.py
+++ python/specfitter.py
@@ -1,6 +1,6 @@
 """A module for classes that perform spectral fitting.
 
-    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/specfitter.py,v 1.6 2009/06/22 23:32:51 kerrm Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/specfitter.py,v 1.7 2009/07/04 15:33:22 burnett Exp $
 
     author: Matthew Kerr
 """
@@ -85,33 +85,89 @@ class SpectralModelFitter(object):
    def hessian(m,mf,*args):
       """Calculate the Hessian; mf is the minimizing function, m is the model,args additional arguments for mf."""
       #p = m.p.copy()
-      p = m.get_parameters().copy()
-      delt = 0.01
-      hessian=N.zeros([len(p),len(p)])
-      for i in xrange(len(p)):
-         for j in xrange(i,len(p)): #Second partials by finite difference
+      p  = m.get_parameters().copy()
+      np = len(p)
+      deltas   = N.abs(0.01 * p) #initial guess
+      hessian  = N.zeros([np,np])
+      bad_mask = N.asarray([False] * np)
+      return_code = N.zeros(np)
+
+      l0 = mf(p,m,*args)
+
+      #find good values with which to estimate the covariance matrix -- look at diagonal deviations
+      #iterate until change in function consistent with ~1 sigma conditional error
+      for i in xrange(np):
+         #print 'Working on parameter %d'%(i)
+         #if p[i] < 0: deltas[i] *= -1 #necessary?
+         h,l = p.copy(),p.copy()
+         for j in xrange(10):
+            h[:] = p[:]; l[:] = p[:];
+            h[i] += deltas[i]
+            l[i] -= deltas[i]
+
+            delta_f_1 = mf(h,m,*args) - l0
+            delta_f_2 = mf(l,m,*args) - l0
+            delta_f = delta_f_1 + delta_f_2 #twice difference, really
+            deltas[i] /= max(delta_f**0.5,0.33) # can change by half decade
+
+            #print delta_f,delta_f_1,delta_f_2
+            
+
+            if delta_f < 5 and delta_f > 0.5: break
+
+         if delta_f < 5e-3:
+            # no constraint on parameter -- ignore it in further fittingor :
+            bad_mask[i] = True
+            return_code[i] = 1
+            #print 'BAAAAAAD 1'
+         if (delta_f_1/delta_f_2 > 10 or delta_f_1/delta_f_2 < 1./10):
+            # significant asymmetry in likelihood          
+            #print 'BAAAAAAD 2'
+            bad_mask[i] = True
+            return_code[i] = 2
+         if (delta_f_2 < 5e-3 and delta_f_1 > 0.5):
+            # not actually at maximum of likelihood -- upper limit condition
+            #print 'BAAAAAAD 3'
+            bad_mask[i] = True
+            return_code[i] = 3
+
+      #print deltas
+         
+      for i in xrange(np):
+         if bad_mask[i]:
+            hessian[i,:] = 0 #no correlation?
+            hessian[:,i] = 0
+            continue
+         for j in xrange(i,np): #Second partials by finite difference
             
             xhyh,xhyl,xlyh,xlyl=p.copy(),p.copy(),p.copy(),p.copy()
-            xdelt = delt if p[i] >= 0 else -delt
-            ydelt = delt if p[j] >= 0 else -delt
+            #xdelt = delt if p[i] >= 0 else -delt
+            #ydelt = delt if p[j] >= 0 else -delt
+            xdelt = deltas[i]
+            ydelt = deltas[j]
 
-            xhyh[i]*=(1+xdelt)
-            xhyh[j]*=(1+ydelt)
+            xhyh[i] += xdelt
+            xhyh[j] += ydelt
 
-            xhyl[i]*=(1+xdelt)
-            xhyl[j]*=(1-ydelt)
+            xhyl[i] += xdelt
+            xhyl[j] -= ydelt
 
-            xlyh[i]*=(1-xdelt)
-            xlyh[j]*=(1+ydelt)
+            xlyh[i] -= xdelt
+            xlyh[j] += ydelt
 
-            xlyl[i]*=(1-xdelt)
-            xlyl[j]*=(1-ydelt)
+            xlyl[i] -= xdelt
+            xlyl[j] -= ydelt
+
+            vals = N.asarray([mf(xhyh,m,*args),-mf(xhyl,m,*args),-mf(xlyh,m,*args),mf(xlyl,m,*args)])
+            #if i == j:
+            #print (N.abs(vals).max() - N.abs(vals).min())
 
             hessian[i][j]=hessian[j][i]=(mf(xhyh,m,*args)-mf(xhyl,m,*args)-mf(xlyh,m,*args)+mf(xlyl,m,*args))/\
-                                          (p[i]*p[j]*4*delt**2)
+                                          (4*xdelt*ydelt)
 
       mf(p,m,*args) #call likelihood with original values; this resets model and any other values that might be used later
-      return hessian
+      
+      return hessian,return_code
 
    @staticmethod
    def gradient(m,mf,*args):
