c3efaa9e797da8ca877c612428244225f54ec7f3
burnett <>
2010-02-03 19:18:34 +0000

    implement limit on localize movement, default 1 deg.

--- python/uw/like/roi_localize.py
+++ python/uw/like/roi_localize.py
@@ -1,7 +1,7 @@
 """
 Module implements localization based on both broadband spectral models and band-by-band fits.
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/roi_localize.py,v 1.4 2009/12/05 18:21:42 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like/roi_localize.py,v 1.1 2010/01/13 20:56:47 kerrm Exp $
 
 author: Matthew Kerr
 """
@@ -12,7 +12,6 @@ from skymaps import SkyDir,Hep3Vector
 from pypsf import PsfOverlap
 
 ###====================================================================================================###
-
 class ROILocalizer(object):
 
    def init(self):
@@ -21,6 +20,7 @@ class ROILocalizer(object):
       self.update    = False
       self.max_iteration=10
       self.bandfits  = True  #default use bandfits
+      self.maxdist   = 1    #fail if try to move further than this
 
    def __init__(self,roi,which=0,**kwargs):
       self.init()
@@ -91,6 +91,9 @@ class ROILocalizer(object):
          delt = l.dir.difference(ps.skydir)*180/N.pi
          sigma = l.par[3]
          if not self.quiet: print ('\t'+7*'%10.4f')% (diff, delt, l.par[0],l.par[1],l.par[3],l.par[4], l.par[6])
+         if delt>self.maxdist:
+            if not self.quiet: print '\t -attempt to move beyond maxdist=%.1f' % self.maxdist
+            raise Exception('roi_localize failure: -attempt to move beyond maxdist=%.1f' % self.maxdist)
          if (diff < tolerance) and (abs(sigma-old_sigma) < tolerance):
             break
          ld = SkyDir(l.dir.ra(),l.dir.dec())
