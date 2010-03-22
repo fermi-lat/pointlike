88237e133e9faa3babf956a6fa4e680a18f8c6ee
burnett <>
2010-03-22 17:28:13 +0000

    some refactoring, better docstrings

--- python/uw/__init__.py
+++ python/uw/__init__.py
@@ -3,8 +3,8 @@ THe UW system for performing spectral analysis
 
 at this top level define a simple "factory" function giving a simple user access to the system
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/__init__.py,v 1.2 2010/03/18 04:52:16 burnett Exp $
-Authors:  T. Burnett, M. Kerr, Eric Wallace, Marshall Roth
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/__init__.py,v 1.3 2010/03/18 20:36:18 burnett Exp $
+Authors:  T. Burnett, M. Kerr, E. Wallace, M. Roth
 """
 
 def factory(**kwargs):
@@ -17,16 +17,16 @@ def factory(**kwargs):
         ==========   =============
         keyword      description
         ==========   =============
-        dataset      None      desciption of data set to useaa; if None, get "all data"
+        dataset      None      desciption of data set to use; if None, get "all data"
         emin         [100]     Default emin for fits
-        free_radius  [0]       fit all sources within this radius
+        free_radius  [1.0]     fit all sources within this radius
         prune_radius [0.1]     Do not include catalog sources within this radius of specified direction
         fit_bg_first [False]   When doing a spectral fit, first optimize the background
         use_gradient [True]    When doing a spectral fit, set the "use_gradient" option
         ==========   =============
     see uw.like.pointspec.SpectralAnalysis docstring for more keywords
     """
-    from thb_roi import myroi
+    from thb_roi import roi_factory
 
     defaults = {
         'dataset'     : None,
@@ -38,5 +38,5 @@ def factory(**kwargs):
         'use_gradient': True,
     }
     defaults.update(kwargs)
-    return myroi.ROIfactory( **defaults )
+    return roi_factory.ROIfactory( **defaults )
     
