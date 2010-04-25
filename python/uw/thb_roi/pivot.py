17e17c46fa4ac8b3de5387df91afd477adedf996
burnett <>
2010-04-25 01:54:52 +0000

    setup documented  module execution, with OptionParser

--- python/uw/thb_roi/pivot.py
+++ python/uw/thb_roi/pivot.py
@@ -1,9 +1,9 @@
 """
 do pivot stuff 
-$Header$
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/thb_roi/pivot.py,v 1.1 2010/04/23 04:13:40 burnett Exp $
 
 """
-
+version='$Revision: 1.1 $'.split()[1]
 from uw.utilities import collection
 from uw.thb_roi import pipeline
 from skymaps import SkyDir
@@ -18,30 +18,29 @@ def near(s, cat, tol=1.0):
     rtol = np.radians(tol)
     return [c for c in cat if sdir.difference(SkyDir(c.ra,c.dec))<rtol and c != s]
 
-def selection(indir):
+def selection(indir, select_all):
     uw = pipeline.load_rec_from_pickles(indir)
-    cut = (uw.band_ts>9)* (uw.qual<25) \
-        * (uw.delta_ts<9) * (uw.pnorm>1e-15) * (uw.pindex>0.5) * (uw.pindex<4)
-    #--- 
-    uwc = uw[cut]
-    print 'selected %d' %len(uwc)
-    return uwc
+     #--- 
+    if not select_all:
+        cut = (uw.band_ts>9)* (uw.qual<25) \
+            * (uw.delta_ts<9) * (uw.pnorm>1e-15) * (uw.pindex>0.5) * (uw.pindex<4)
+        uwc = uw[cut]
+        print 'selected %d' %len(uwc)
+        return uwc
+    return uw
 
-def makepivot(indir, outdir, name,  outfile='pivot.cxml' ,datarec=None, img_list=None, dzc='dzc.xml',
-     href=None, linkname=None):
+def makepivot(indir, outdir, name, select_all=False, outfile='pivot.cxml', img_list=None, dzc='dzc.xml',
+        href=None, linkname=None,
+        ):
     """
-     indir: where to find the pickle files describing the sources, unless overridden by datarec
-     datarec:  recarray 
+     indir: where to find the pickle files describing the sources, u
      outdir: where to setup the pivot files: expect to find a dzc file here with the names
      name:  name for the Pivot Collection
     """
     if not os.path.exists(outdir): 
         raise InvalidParameter('folder %s not found')
     fulloutfile = os.path.join(outdir, outfile)
-    if datarec is not None:
-        uwc = datarec
-    else:
-        uwc = selection(indir)
+    uwc = selection(indir, select_all)
     sdir = map(SkyDir, uwc.ra, uwc.dec)
     glon = np.asarray([s.l() for s in sdir])
     cglon = glon.copy()
@@ -72,7 +71,7 @@ def makepivot(indir, outdir, name,  outfile='pivot.cxml' ,datarec=None, img_list
     dts[dts<1e-3]=0
     col.add_facet('delta_ts', 'Number', 'F1', dts)
     col.add_facet('id_prob', 'Number', 'F3', uwc.id_prob)
-    col.add_facet('class',  'String', 'C3', ['%3s'% s for s in uwc.aclass])
+    col.add_facet('class',  'String', 'C3', ['%3s'% s if s!='' else 'None' for s in uwc.aclass])
 
     if href is not None:
         hreflist = [href % n for n in uw.name[cut]]
@@ -95,11 +94,19 @@ def makepivot(indir, outdir, name,  outfile='pivot.cxml' ,datarec=None, img_list
     print 'done!'
 
 
-
 if __name__=='__main__':
-    argv = sys.argv
-    if len(argv)!=4: raise InvalidParameter('expect 3 parameters: indir, outdir, collection_name')
-    indir = argv[1]
-    outdir= argv[2]
-    collection_name = argv[3]
-    makepivot(indir, outdir, collection_name)
+    from optparse import OptionParser
+    usage = """\n
+usage: %prog [options] indir outdir name\n
+Generate pivot collection
+    indir: where to find the pickle files describing the sources
+    outdir: where to setup the pivot files: expect to find a dzc file here with the names
+    name:  name for the Pivot Collection (surround by double quotes if more than one token) """
+    parser = OptionParser(usage, version=version)
+    parser.add_option('-a', '--all', help='keep all sources', action='store_true', dest='select_all',default=False)
+    options, args = parser.parse_args()
+    if len(args)!=3: 
+        parser.print_usage()
+        sys.exit(-1)
+    makepivot(args[0],args[1],args[2], select_all=options.select_all)
+ 
