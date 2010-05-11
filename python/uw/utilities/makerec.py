14382c6886b50355de6e9890f43e90a9d8fd593b
burnett <>
2010-05-11 18:58:42 +0000

    convenience load function does either pickle or txt

--- python/uw/utilities/makerec.py
+++ python/uw/utilities/makerec.py
@@ -1,12 +1,11 @@
 """ Various useful utilities for creating, dumping numpy recarry objects
-$Header:$
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/utilities/makerec.py,v 1.2 2010/04/23 04:16:44 burnett Exp $
 
 
 
 """
-import matplotlib
+import os, pyfits, pickle, matplotlib
 import numpy as np
-import pyfits
 
 def makefits(r, filename=None):
     """ convert a recarray to a pyfits object, write to filename if present
@@ -15,7 +14,7 @@ def makefits(r, filename=None):
         if dtype[:2]=='|S': return dtype[2:]+'A'
         try:
             return {'<f8':'D', '<f4':'E', '<i4':'J', '|b1':'L', '|O4':'|O4',}[dtype]
-        except:
+        except KeyError:
             print 'recarry type %s not recognized' %dtype
             raise
     def column(n,f):
@@ -90,4 +89,11 @@ class RecArray(object):
         """ return finished recarray"""
         return np.rec.fromarrays(self.fields, names=self.names)
 
-        
+      
+def load(filename):
+    ext = os.path.splitext(filename)[1]
+    if ext=='.txt':
+        return textrec(filename)
+    elif ext=='.pickle':
+        return pickle.load(open(filename))
+    raise Exception('extension %s not recognized: expect either txt or pickle' %ext)
