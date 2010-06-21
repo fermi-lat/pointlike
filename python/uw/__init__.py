4d177e80ac757fe4c96e95f97a954748ecbd5948
burnett <>
2010-06-21 19:52:32 +0000

    more tweaks to be location-independent

--- python/uw/__init__.py
+++ python/uw/__init__.py
@@ -3,13 +3,13 @@ THe UW system for performing spectral analysis
 
 at this top level define a simple "factory" function giving a simple user access to the system
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/__init__.py,v 1.4 2010/03/22 17:28:13 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/__init__.py,v 1.5 2010/06/20 13:42:35 burnett Exp $
 Authors:  T. Burnett, M. Kerr, E. Wallace, M. Roth
 """
 
 def factory(**kwargs):
 
-    from thb_roi import roi_factory, config
+    from thb_roi import roi_factory
     import os
 
 #    f = roi_factory.ROIfactory(config.AE(**kwargs))
