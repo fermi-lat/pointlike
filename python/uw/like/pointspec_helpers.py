15b211796bddc5b2d4d189d176f637417de18c63
lande <>
2010-08-02 20:44:56 +0000

    Stripwhite space from catalog source names.

--- python/uw/like/pointspec_helpers.py
+++ python/uw/like/pointspec_helpers.py
@@ -1,5 +1,5 @@
 """Contains miscellaneous classes for background and exposure management.
-   $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like/pointspec_helpers.py,v 1.15 2010/07/06 23:01:05 lande Exp $
+   $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/pointspec_helpers.py,v 1.16 2010/07/20 23:17:05 kerrm Exp $
 
    author: Matthew Kerr
    """
@@ -225,7 +225,7 @@ class FermiCatalog(PointSourceCatalog):
       self.dirs   = map(SkyDir,N.asarray(ras).astype(float),N.asarray(decs).astype(float))
       self.models = N.asarray([PowerLaw(p=[n0,ind],e0=pen) for n0,ind,pen in zip(n0s,inds,pens)])
       #self.fluxes = N.asarray(f[1].data.field('FLUX100'))
-      self.names  = N.asarray(f[1].data.field(sname))
+      self.names  = N.char.strip(f[1].data.field(sname))
       #self.ts     = N.asarray(ts)
 
       f.close()
