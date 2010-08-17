f443b9d6cc7fee94effd94067526c43598135d2d
burnett <>
2010-08-17 17:57:05 +0000

    major clean up; interpret prune_radius as radius of catalog sources to exclude from model

--- python/uw/thb_roi/roi_setup.py
+++ python/uw/thb_roi/roi_setup.py
@@ -1,7 +1,7 @@
 """
 supplemental setup of ROI
 ----------------------------------
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/thb_roi/roi_setup.py,v 1.7 2010/06/20 13:39:30 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/thb_roi/roi_setup.py,v 1.8 2010/08/06 18:15:11 burnett Exp $
 
 These are near-duplicates of the classes with the same name in uw.like, but modifed for the interactive access
 
@@ -10,39 +10,15 @@ import numpy as np
 import os, pyfits, types, pickle
 
 from uw.utilities import makerec
-from uw.like import Models, roi_managers, pointspec_helpers
-from skymaps import SkyDir, DiffuseFunction, IsotropicSpectrum
+from uw.like import Models,  pointspec_helpers
+from skymaps import SkyDir
 
-class ConsistentBackground(object):
-    """A factory for constructing a  background model for a fit
-
-   """
-
-    def __init__(self, diffdir, quiet=False):
-        fb = ('front','back')
-        self.dmodels = ( 
-           [ DiffuseFunction(os.path.join(diffdir,'gll_iem_v02_P6_v8_diff_%s.fits'%c)) for c in fb], 
-           [ IsotropicSpectrum(os.path.join(diffdir,'isotropic_iem_%s_v02.txt'%c)) for c in fb],
-           )
-        self.names = ('gll_iem_v02', 'Isotropic Diffuse')
-
-    def __call__(self, skydir=None, models = None, lat = None):
-        """
-        return a new set of background models
-        """
-        smodels = [ Models.PowerLaw(p=[1,1],free=[True,True],index_offset=1),
-                    Models.Constant(free=[True]) ]
-
-        # attempt to make index free depending on lat??
-        smodels[0].free[1] = (lat is not None and abs(lat) < 20) 
-
-        smodels = smodels if models is None else models
-        return map(roi_managers.ROIBackgroundModel, self.dmodels, smodels, self.names)
-      
+     
 class CatalogManager(object):
     """Read a  catalogue and use it to set up a source list for a ROI."""
     defaults = dict(
         prune_radius  = 0.10,   #deg; in a merge, consider sources closer than this duplicates
+                                # or to exclude all sources within a larger region for separate specification
         free_radius   = 1,      #deg; sources within this distance have free spectral parameters
         min_flux      = 2e-9,   #ph/cm2/s; minimum flux for sources beyond a certain proximity
         max_distance  = 5,      #deg; distance inside which sources are returned regardless of flux
@@ -58,20 +34,24 @@ class CatalogManager(object):
             if key in self.__dict__: self.__dict__[key]=value
             else:
                 print 'warning: key %s not recognized by CatalogManager' % key
-        #self.__dict__.update(kwargs)
         if not self.quiet: 
-            print 'creating a CatalogManager'
+            print 'creating a CatalogManager from %s...' %catalog_file
         cdata = pyfits.open(catalog_file)[1].data
         try:
             ts   = np.asarray(cdata.field('Test_Statistic'),float)
         except KeyError:
             ts   = np.asarray(cdata.field('Signif_Avg'))**2
         good = ts>self.min_ts
+             
         ras  = np.asarray(cdata.field('RA'),float)[good]
         decs = np.asarray(cdata.field('DEC'),float)[good]
         pens = cdata.field('PIVOT_ENERGY')[good]
         n0s  = cdata.field('FLUX_DENSITY')[good]
         inds = cdata.field('SPECTRAL_INDEX')[good]
+        try:
+            cuts = cdata.field('Cutoff_Energy')[good]
+        except KeyError:
+            cuts = good.sum()*[np.nan]
         inds = np.where(inds > 0, inds, -inds)
         try:
             self.names  = np.asarray(cdata.field('Source_Name'))[good]
@@ -85,10 +65,12 @@ class CatalogManager(object):
             if type(self.pulsar_dict) == types.StringType:
                 self.pulsar_dict = pickle.load(open(self.pulsar_dict))
             pdkeys = self.pulsar_dict.keys()
-        def load_model(name, n0,ind, pen):
+        def load_model(name, n0,ind, pen, cut):
             #if name[:3]=='PSR': assert False, 'breakpoint'
             if name not in pdkeys:
-                return Models.PowerLaw(p=[n0,ind],e0=pen)
+                return Models.PowerLaw(p=[n0,ind],e0=pen) if np.isnan(cut) or cut==0 else\
+                       Models.ExpCutoff(p=[n0,ind,cut],e0=pen)
+            # Use pulsar_dict to override catalog           
             psr = self.pulsar_dict[name]
             if psr['TS']<100:return Models.PowerLaw(p=[n0,ind],e0=pen)
             stat = psr['stat'][0]
@@ -97,9 +79,12 @@ class CatalogManager(object):
                     % ((name,) + tuple(stat) )
             return Models.ExpCutoff(p=stat)
             
-        self.models = np.asarray([load_model(name.strip(),n0,ind,pen) for name,n0,ind,pen in zip(self.names,n0s,inds,pens)])
+        self.models = np.asarray([load_model(name.strip(),n0,ind,pen,cut) for name,n0,ind,pen,cut in zip(self.names,n0s,inds,pens,cuts)])
         if not self.quiet: 
-            print 'Loaded %d sources from catalog "%s" for roi backgrounds' % (len(cdata), catalog_file)
+            print 'Loaded %d sources  for roi backgrounds' % (len(cdata),)
+            if np.sum(-good)>0:
+                print '\tNot using %d entries with TS<25' % np.sum(-good)
+
             if self.pulsar_dict is not None:
                 print '\tUsing a pulsar dictionary with %d entries' % len(pdkeys)
 
@@ -120,11 +105,15 @@ class CatalogManager(object):
  
         
     def __call__(self,skydir, radius):
+        """ return sources, as a list of PointSource objects, within radius of skydir
+            set as free those within free_radius
+            exclude those within prune_radius
+        """
 
         diffs   = np.degrees(np.asarray([skydir.difference(d) for d in self.dirs]))
         #mask    = ((diffs < radius)&(self.ts > self.min_ts)) & \
         #          ((self.fluxes > self.min_flux)|(diffs < self.max_distance))         
-        mask    = diffs < radius
+        mask    = (diffs < radius) 
         diffs   = diffs[mask]
         sorting = np.argsort(diffs)
 
@@ -135,18 +124,19 @@ class CatalogManager(object):
         names   = self.names[mask][sorting]
         models  = [x.copy() for x in self.models[mask][sorting]]
 
-        fm = (diffs[sorting] < self.free_radius) 
-        point_sources = map(pointspec_helpers.PointSource,dirs,names,models,fm)
+        # now select those in annulus between prune_radius and free_radius for inclusion in the model
+        exclude = (diffs[sorting] < self.prune_radius) 
+        numex = exclude.sum()
+        makefree = (diffs[sorting] < self.free_radius) 
+        point_sources = map(pointspec_helpers.PointSource,dirs,names,models,makefree)
         if not self.quiet:
-            print '...selected %d sources within %.1f deg for roi' % (len(point_sources), radius)
-            print '...selected %d sources within %.1f deg for refit' % (fm.sum(), self.free_radius)
-        if diffs[sorting][0]< self.prune_radius: 
-            if not self.quiet:
-                print '...exclude catalog source %s  closer than %.1f' % (names[0], self.prune_radius)
-            self.exclude = point_sources[0] 
-            return point_sources[1:]
-        self.exclude = None 
-        return point_sources
+            print '...selected %d sources within %.1f deg for roi'   % (len(point_sources), radius)
+            print '...selected %d sources within %.1f deg for refit' % (makefree.sum(), self.free_radius)
+            if numex>0:
+                print '...excluded %d sources within %.1f deg : %s ' % (numex, self.prune_radius, ', '.join([s.name.strip() for s in point_sources[:numex]]))
+        
+        self.exclude= point_sources[:numex]
+        return point_sources[numex:]
 
     def merge_lists(self,skydir,radius=15,user_list=None):
         """Get a list of catalog sources and merge it with an (optional) list of PointSource objects
@@ -184,31 +174,7 @@ class CatalogManager(object):
                         names = 'name ra dec pnorm pindex'.split(),
                        )
 
-def UpdateRoiManager(object):
 
-    """
-    wrapper around the ROI class
-    
-    """
-    def __init__(self, roi_manager):
-        """
-        roi_analysis
-        """
-        self.roi_manager = roi_manager
-        self.ps_manager = roi_manager.ps
-        
-    def add_source(self, name, dir, model):
-        rm = self.roi_manager
-        rm.names.append(dir)
-        rm.dirs.append(dir)
-        rm.models.append(model)
-        
-    def add_source_array(self, recarray):
-        """
-        recarray: record array of sources to add
-        """
-        for s in recarray:
-            add_source(s.name, SkyDir(s.ra,s.dec), Models.PowerLaw(p=(s.no,s.ind),e0=1000.))
 
 if __name__=='__main__':
     pass
