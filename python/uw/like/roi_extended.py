398aad4c03d205a817e4dac272139e258d7fc6c9
lande <>
2010-08-27 06:10:34 +0000

    Increase r_multi for more accurate spectral analysis

--- python/uw/like/roi_extended.py
+++ python/uw/like/roi_extended.py
@@ -2,7 +2,7 @@
 
     This code all derives from objects in roi_diffuse.py
 
-    $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_extended.py,v 1.20 2010/08/25 00:33:38 lande Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_extended.py,v 1.21 2010/08/27 03:03:11 lande Exp $
 
     author: Joshua Lande
 """
@@ -108,7 +108,7 @@ class ROIExtendedModel(ROIDiffuseModel):
     def init(self):
         self.pixelsize = 0.025
         self.npix      = 101
-        self.r_multi   = 1.0 # multiple of r95 to set max dimension of grid
+        self.r_multi   = 2.0 # multiple of r95 to set max dimension of grid
         self.r_max     = 20  # an absolute maximum (half)-size of grid (deg)
 
     def setup(self):
