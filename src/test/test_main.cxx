3c8a13963a38a05804cdfd6415adfe7b1c19b9cb
burnett <>
2010-12-10 18:49:54 +0000

    disable more

--- src/test/test_main.cxx
+++ src/test/test_main.cxx
@@ -107,11 +107,11 @@ int main(int argc , char** argv )
 
 
         std::vector<astro::SkyDir> directions;
-
+#if 0 //?
         // test fitting with a diffuse component
         skymaps::SkySpectrum* diffuse(new skymaps::IsotropicPowerLaw());
         PointSourceLikelihood::set_diffuse(diffuse);
-
+#endif
         for( int n=0; !points[n].name.empty(); ++n){
                         
             astro::SkyDir dir(points[n].ra,points[n].dec);
