67fcb8ec7f162b16168db90077d8addd6c364412
lande <>
2011-02-03 20:25:30 +0000

    adding docstring for AnalyticConvolutionCache

--- python/uw/utilities/convolution.py
+++ python/uw/utilities/convolution.py
@@ -1,6 +1,6 @@
 """Module to support on-the-fly convolution of a mapcube for use in spectral fitting.
 
-$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/utilities/convolution.py,v 1.31 2011/02/01 05:33:32 lande Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/utilities/convolution.py,v 1.32 2011/02/03 01:24:09 lande Exp $
 
 authors: M. Kerr, J. Lande
 
@@ -649,6 +649,9 @@ class AnalyticConvolutionCache(AnalyticConvolution):
         the same fitpsf value) as the last time, the previous value is
         returned instead of being recalculated. """
 
+    defaults = AnalyticConvolution.defaults
+
+    @keyword_options.decorate(defaults)
     def __init__(self,*args,**kwargs):
 
         super(AnalyticConvolutionCache,self).__init__(*args,**kwargs)
