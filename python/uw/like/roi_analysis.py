2920fcb7e8f9b942fafbb2d92b80875d549d0f49
lande <>
2011-03-19 00:22:35 +0000

    Bugfix for analytic gradient with no point sources

--- python/uw/like/roi_analysis.py
+++ python/uw/like/roi_analysis.py
@@ -2,7 +2,7 @@
 Module implements a binned maximum likelihood analysis with a flexible, energy-dependent ROI based
     on the PSF.
 
-$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_analysis.py,v 1.73 2011/03/11 22:46:48 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_analysis.py,v 1.74 2011/03/17 23:40:09 lande Exp $
 
 author: Matthew Kerr
 """
@@ -233,7 +233,7 @@ class ROIAnalysis(object):
             self.update_counts(parameters)
 
         # do the point sources
-        indices  = N.arange(len(models))[N.asarray([N.any(m.free) for m in models])]
+        indices  = N.arange(len(models))[[N.any(m.free) for m in models]]
         nparams  = N.asarray([model.free.sum() for model in models])
         gradient = N.zeros(nparams.sum())
 
