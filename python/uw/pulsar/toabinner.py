0b7be515e6c92d4b752609628511172540641217
kerrm <>
2011-06-18 00:17:10 +0000

    Removed significance binner; added one based on template that works MUCH better.

--- python/uw/pulsar/toabinner.py
+++ python/uw/pulsar/toabinner.py
@@ -1,5 +1,5 @@
 """
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like/roi_analysis.py,v 1.84 2011/04/20 00:36:30 lande Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/pulsar/toabinner.py,v 1.1 2011/04/27 18:32:03 kerrm Exp $
 
 handle binning of data for extraction of TOAs
 
@@ -7,17 +7,17 @@ Authors: Paul S. Ray <paul.ray@nrl.navy.mil>
          Matthew Kerr <matthew.kerr@gmail.com>
 """
 import numpy as np
-from stats import hm,hmw
 import sys
 
 class TOABinner(object):
     """ Implement an interface to calculate bin edges for a span of LAT
         observations.  Note the default implementation is uniform in time."""
 
-    def __init__(self,ntoa,phase_data):
+    def __init__(self,ntoa,phase_data,template=None):
         self.counter = 0
         self.ntoa = ntoa
         self.data = phase_data
+        self.template = template # not needed for standard calculation
         self.make_edges()
 
     def __iter__(self):
@@ -33,87 +33,37 @@ class TOABinner(object):
         bin_edges = np.linspace(self.data.mjd_start,self.data.mjd_stop,self.ntoa+1)
         self.starts = bin_edges[:-1]
         self.stops  = bin_edges[1:]
-            
-class UniformSigBinner(TOABinner):
-    """ Attempt to select bin edges such that each bin has approximately the same pulsed
-        significance as measured by the H test.
 
-        TODO -- would be better to use a Z^2_m test to reduce fluctuations
-        TODO -- a better approach in general is to pick a goal for the H (Z^2)
-            test and pick the number of bins accordingly, rather than vice
-            versa as current implementation.
-    """
+class UniformLogBinner(TOABinner):
+    """ Uses the information in the profile to bin the data in uniform "log drops".
+        This is based on a likelihood ratio test for the pulsation significance
+        and should do a good job of dividing the data up into regions that give
+        approximately equal power in estimating a TOA."""
     
     def make_edges(self):
         phases,weights,mjds = self.data.ph,self.data.weights,self.data.mjds
-        ntoa = self.ntoa
-        def get_hs():
-            if weights is None:    
-                return np.asarray([hm(phases[i0[i][0]:i0[i][1]]) for i in xrange(ntoa)])
+        logs = np.log(self.template(phases)) -np.log(1-self.template.norm())
+        delta = logs.sum()/self.ntoa
+        my_delta = 0
+        index = 1
+        indices = np.asarray([-1]*(self.ntoa+1))
+        for i in xrange(len(phases)):
+            if my_delta > delta:
+                indices[index] = i
+                index += 1
+                my_delta = my_delta - delta
+            my_delta += logs[i]
+        indices[-1] = len(phases)-1
+        indices[0] = 0
+        assert(not np.any(indices==-1))
+        self.starts = np.empty(self.ntoa); self.stops = np.empty_like(self.starts)
+        for i in xrange(self.ntoa):
+            if i == 0:
+                self.starts[i] = mjds[0] - 1
             else:
-                return np.asarray([hmw(phases[i0[i][0]:i0[i][1]],weights[i0[i][0]:i0[i][1]]) for i in xrange(ntoa)])
-        e0 = np.linspace(self.data.mjd_start,self.data.mjd_stop,self.ntoa+1)
-        idx = np.arange(len(phases))
-        i0 = []
-        for i in xrange(ntoa):
-            indices = idx[(mjds>e0[i])&(mjds<e0[i+1])]
-            i0 += [ [indices[0],indices[-1]] ]
-        print [x[1]-x[0] for x in i0]
-        init_hs = get_hs()
-        print 'Initial set of H-test values with std dev. %.2f'%(init_hs.std())
-        print init_hs
-        shift = max(1,int(float(len(phases))/ntoa*0.02))
-        def do_shift(my_shift,idx):
-            if idx == 0:
-                i0[idx][1] += 2*my_shift
-                i0[idx+1][0] += 2*my_shift
-            elif idx == ntoa-1:
-                i0[idx][0] -= 2*my_shift
-                i0[idx-1][1] -= 2*my_shift
+                self.starts[i] = (mjds[indices[i]] + mjds[indices[i]-1])/2
+            if i == (self.ntoa-1):
+                self.stops[i] = mjds[-1] + 1
             else:
-                i0[idx][0] -= my_shift
-                i0[idx][1] += my_shift
-                i0[idx-1][1] -= my_shift
-                i0[idx+1][0] += my_shift
-        goal = 0.1*init_hs.mean()
-        old_std = 0
-        old_std2 = 0
-        def check_indices():
-            for idx in i0:
-                if idx[1]-idx[0] <= 10: return False 
-            return True
-
-        for j in xrange(300):
-            hs = get_hs()
-            std = hs.std()
-            if std < goal:
-                break
-            #print std;sys.stdout.flush()
-            if (std == old_std) or (std == old_std2):
-                print 'Detected loop'
-                shift -= 1
-                if shift==0: shift = 2
-                shift = max(1,shift)
-            old_std2,old_std = old_std,std
-            m = hs.mean()
-            for i,h in enumerate(hs):
-                my_shift = shift if (h < m) else -shift
-                do_shift(my_shift,i)
-                if not check_indices(): do_shift(-my_shift,i)
-                ph = phases[i0[i][0]:i0[i][1]]
-                nh = hm(phases[i0[i][0]:i0[i][1]])
-                if ((h < m) and ((nh+1) < h)) or ((h >= m) and ((nh-1) > h)):
-                    #print i,h,nh,m
-                    do_shift(-my_shift,i)
-        print 'Final set of H-test values with std. dev %.2f'%(hs.std())
-        print hs
-
-        # construct MJD bounds
-        edges = np.empty(ntoa+1)
-        edges[0] = self.data.mjd_start
-        edges[-1] = self.data.mjd_stop
-        for i in xrange(ntoa-1):
-            edges[i+1] = (mjds[i0[i][1]] + mjds[i0[i+1][0]])/2
-        #return edges
-        print edges
-        self.starts = edges[:-1]; self.stops = edges[1:]
+                self.stops[i] = (mjds[indices[i+1]] + mjds[indices[i+1]-1])/2
+            
