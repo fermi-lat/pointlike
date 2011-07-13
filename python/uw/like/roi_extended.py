095170a0b545706c879a7c4f4042b9686e3871fe
lande <>
2011-07-13 03:19:21 +0000

    Remove free_parameters and leave_parameters flags since they seem pointless. Better convergence strategy in fit_extension by trying again also at the best likelihood values. Bugfix for TS_ext function. Remove fit_psf and fast_psf flags since they no longer work.

--- python/uw/like/roi_extended.py
+++ python/uw/like/roi_extended.py
@@ -2,7 +2,7 @@
 
     This code all derives from objects in roi_diffuse.py
 
-    $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_extended.py,v 1.59 2011/07/07 21:23:26 lande Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_extended.py,v 1.60 2011/07/08 21:34:27 lande Exp $
 
     author: Joshua Lande
 """
@@ -30,13 +30,6 @@ class ExtendedSource(DiffuseSource):
         ('model',None,'a Model object.'),
         ('spatial_model',None,"""The spatial model to use. 
                                        This is a SpatialModel object."""),
-        ('free_parameters',True,"""Set all spectral model and 
-                                   spatial_model parameter's free 
-                                   status to this value"""),
-        ('leave_parameters',False,"""Don't set any of the spectral 
-                                     or spatial parameter's free status. 
-                                     Leave them at whatever their default 
-                                     values are.""")
     )
 
     @keyword_options.decorate(defaults)
@@ -60,10 +53,6 @@ class ExtendedSource(DiffuseSource):
 
         self.model.background = False
 
-        if not self.leave_parameters:
-            for i in xrange(len(self.model.free)): self.model.free[i] = self.free_parameters
-            for i in xrange(len(self.spatial_model.free)): self.spatial_model.free[i] = self.free_parameters
-
     @property
     def skydir(self): return self.spatial_model.center
 
@@ -83,8 +72,7 @@ class ExtendedSource(DiffuseSource):
         """ Create a deep copy of an extended source. """
         return ExtendedSource(name=self.name,
                               spatial_model=self.spatial_model.copy(),
-                              model=self.model.copy(),
-                              leave_parameters=True)
+                              model=self.model.copy())
 
 
 ###=========================================================================###
@@ -270,13 +258,19 @@ Arguments:
 
         # keep the roi quiet during fit, so lots of print is supressed during the iteration.
         roi.old_quiet = roi.quiet
-        roi.quiet = True
+        roi.quiet = False
 
         if roi.TS(which=self.name,quick=True,bandfits=bandfits) < 1:
             raise Exception("Unable to localize a source with initial TS<1")
 
-        init_spectral = es.model.get_parameters()
+        init_spectral = roi.parameters().copy()
         init_spatial = sm.get_parameters(absolute=False)
+        ll_0 = 0
+
+        # Fix scoping issue in likelihood_wrapper
+        # In python 3, could use 'nonlocal' parameter.
+        # http://eli.thegreenplace.net/2011/05/15/understanding-unboundlocalerror-in-python/
+        d=dict(best_spectral = roi.parameters().copy(), ll_best = 0)
 
         if not np.any(sm.free):
             raise Exception("Unable to fit the diffuse source %s's extension. No parameters to fit. Perhaps when you wrote your xml file, you forgot to set some of the spatial parameters to be free?" % es.name)
@@ -303,17 +297,12 @@ Arguments:
 
                 Note: Sometimes the fitter gets confused. For example,
                 if the last iteration moved to a spatial value really far
-                from the initial position, the source flux will get fit
-                to 0 (as it should), but during the next iteration at a
-                more reasonable spatial value, the fit will be unable
-                to reconverge on the best flux and stays at 0. To get
-                around this, I cache the initial fit value (which is
-                presumably pretty good), and whenever the current fit
-                logLikelihood is less then the initial logLikelihood,
-                I rest the loglikelihood to
-                
-                N.B. roi.dsm.update_counts handled by fit() calling logLikelihood()
-                """
+                from the initial position, the model will very pooorly
+                converge and during the next fill be unable to move
+                back to reasonable spectral values.  To get around this,
+                the code remembers the initial and best spectral values
+                and tries those out whenever the overall likelihood is
+                worse than it was before.  """
             p=p.copy()
 
             lon,lat=p[0:2]
@@ -344,26 +333,52 @@ Arguments:
             if bandfits:
                 ll=roi.bandFit(es)
             else:
+                roi.print_summary()
                 ll=roi.fit(estimate_errors=False,use_gradient=use_gradient)
 
-                if ll < ll_0:
-                    prev_fit=es.model.get_parameters()
-                    es.model.set_parameters(init_spectral)
+                roi.print_summary()
+
+                if ll < d['ll_best']:
+                    prev_fit=roi.parameters().copy()
+                    roi.set_parameters(d['best_spectral'].copy())
+                    roi.__update_state__()
                     ll_alt=roi.fit(estimate_errors=False,use_gradient=use_gradient)
 
-                    if ll_alt > ll: ll=ll_alt
-                    else: es.model.set_parameters(prev_fit)
+                    roi.print_summary()
+
+                    if ll_alt > ll: 
+                        ll=ll_alt
+                    else: 
+                        roi.set_parameters(prev_fit.copy())
+                        roi.__update_state__()
+
+                if ll < ll_0:
+                    prev_fit=roi.parameters().copy()
+                    roi.set_parameters(init_spectral.copy())
+                    roi.__update_state__()
+                    ll_alt=roi.fit(estimate_errors=False,use_gradient=use_gradient)
+
+                    roi.print_summary()
+
+                    if ll_alt > ll: 
+                        ll=ll_alt
+                    else: 
+                        roi.set_parameters(prev_fit)
+                        roi.__update_state__()
+
+                if ll > d['ll_best']: 
+                    d['ll_best'] = ll
+                    d['best_spectral'] = roi.parameters().copy()
 
             if not self.quiet: print '%s, logL = %.3f, dlogL = %.3f' % (sm.pretty_string(),ll,ll-ll_0)
-            return -ll
+            return -ll # return negative log likelihood, suitable for minimizing
 
         f=likelihood_wrapper
 
-        ll_0 = 0
 
         # shut up print just the first time
         old_quiet = self.quiet; self.quiet = True; 
-        ll_0 = -f(init_spatial); 
+        ll_0 = d['ll_best'] = -f(init_spatial); 
         self.quiet = old_quiet
 
         if not self.quiet: print 'Localizing %s source %s Using %s' % (sm.pretty_name,es.name,
@@ -455,6 +470,9 @@ Arguments:
         es = self.extended_source
         sm = es.spatial_model
 
+        if not sm.can_shrink():
+            raise Exception("Unable to calculate ts_ext for %s source %s. No way to shrink to null hypothesis." % (sm.pretty_name,es.name))
+
         old_sm_p       = sm.p.copy()
         old_sm_cov     = sm.cov_matrix.copy()
         old_sm_free    = sm.free.copy()
@@ -475,9 +493,7 @@ Arguments:
 
         ll_disk = l()
 
-        if not sm.can_shrink():
-            raise Exception("Unable to calculate ts_ext for %s source %s. No way to shrink to null hypothesis." % (sm.pretty_name,es.name))
-
+        sm.shrink()
         self.initialize_counts(roi.bands)
 
         if not roi.quiet: print 'Refitting position for the null hypothesis'
@@ -486,7 +502,7 @@ Arguments:
 
         if not roi.quiet: print 'Redoing spectral fit in the null hypothesis'
 
-        f() # have to refit in case fitpsf or bandfits was used during the localization.
+        f() # have to refit in case bandfits was used during the localization.
 
         ll_point = l()
 
@@ -517,21 +533,11 @@ class ROIExtendedModelAnalytic(ROIExtendedModel):
         Any of the optional keyword arguments to uw.utilities.convolution's 
         AnalyticConvolutionCache class will be passed on to that class.  """
 
-    defaults = ROIExtendedModel.defaults + (
-        ['fitpsf',False,'Method where the psf is calculated by fitting a single king function to the actual psf.'],
-        ['fastpsf',False,'Method where the psf is apprximated by weighting the sigmas and gammas before evaluating the psf.']
-    )
-
-    @staticmethod
-    def set_fastpsf(fastpsf):
-        was = ROIExtendedModelAnalytic.defaults[1][1]
-        ROIExtendedModelAnalytic.defaults[1][1] = fastpsf
-        return was
+    defaults = ROIExtendedModel.defaults
 
     @keyword_options.decorate(defaults)
     def __init__(self,*args,**kwargs):
         super(ROIExtendedModelAnalytic,self).__init__(*args,**kwargs)
-        self.already_fit=False
         self.overlap = PsfOverlap()
 
     def setup(self):
@@ -542,7 +548,7 @@ class ROIExtendedModelAnalytic(ROIExtendedModel):
         self.active_bgc = AnalyticConvolutionCache(self.extended_source,psf,**d)
 
     def set_state(self,band):
-        self.active_bgc.do_convolution(band,self.fitpsf, self.fastpsf)
+        self.active_bgc.do_convolution(band)
         self.current_energy = energy=band.psf.eopt if band.psf.__dict__.has_key('eopt') else band.e
 
     def _pix_value(self,pixlist):
@@ -558,190 +564,6 @@ class ROIExtendedModelAnalytic(ROIExtendedModel):
                             radius_in_rad=radius if radius is not None else band.radius_in_rad,
                             override_pdf=self.active_bgc,
                             override_integral=self.active_bgc.integral)
-
-    def initialize_counts(self,bands,roi_dir=None):
-        # probably there is a cleaner way to do this, but since you should only 
-        # use this feature in the process of localizing one source, it is 
-        # probably good enough.
-
-        if self.fitpsf and not self.already_fit: 
-            # Note that this must be done before calculating the pdf.
-            fitter=BandFitter(bands,self.quiet)
-            fitter.fit()
-            self.already_fit=True
-
-        super(ROIExtendedModelAnalytic,self).initialize_counts(bands,roi_dir)
-
-    def fit_extension(self,*args,**kwargs):
-        """ Localiztion of radially symmetric extended sources is exactly the same as
-            regular extended sources. The only difference is radially symmetric
-            sources can be fit with the addition of an optional flag 'fitpsf'.
-
-  =========   =======================================================
-  Keyword     Description
-  =========   =======================================================
-  fitpsf        [True]  Use an approximation to the PSF in each energy bin
-                        where PSF is weighted in each energy bin by an assumed
-                        spectral index and is fit by a single king function
-                        which is then convovled with the extended source shape.
-                        This leads to a significantly faster source localization.
-  =========   =======================================================
-        """
-        if kwargs.has_key('roi'):
-            roi = kwargs['roi']
-        else:
-            roi=args[0]
-
-        fitpsf=True if self.fastpsf!=True else False
-        if kwargs.has_key('fitpsf'):
-            fitpsf = kwargs.pop('fitpsf')
-
-        if fitpsf:
-            if self.fastpsf: raise Exception("Cannot specify fitpsf for localization since fastpsf is already selected.")
-
-            if not roi.quiet: print 'Changing to fitpsf for localization step.'
-            self.fitpsf,old_fitpsf=True,self.fitpsf
-
-        stuff = super(ROIExtendedModelAnalytic,self).fit_extension(*args,**kwargs)
-
-        if fitpsf:
-            if not roi.quiet: print 'Setting back to original PDF accuracy.'
-            self.fitpsf=old_fitpsf
-            self.initialize_counts(roi.bands)
-            roi.update_counts()
-        return stuff
-
-class BandFitter(object):
-    """ This class has a somewhat weird purpose. Basically the PSF is
-        a king function, (or for the newpsf two king functions). But
-        this psf shape is a function of energy and the incident theta
-        angle of the photos. When studying extended sources, we have a
-        semianalytic formula to convolve a king function with the shape
-        we are interested in studying. On the other hand, pointlike
-        does a binned analysis binning in course energy bins and not
-        in photon theta angle. So when calculating an extended source
-        model counts, we must convolve our spatial model with the PSF
-        for different theta and energy values in a particular energy
-        bin and then integrate over energy & theta weighting by the LAT's
-        exposure and the spectral model's spectrum. For point sources,
-        the PSF is integrated over 16 sub energy values and 8 theta
-        values for each energy bin to get the model predicted counts.
-        But to study extended sources, for each band doing 16x8 of these
-        convolutions becomes untenable in the process of fitting the
-        spatial extension of the source.
-
-        To work around this, we are interested in 'faking' by calculating
-        what the psf truly should by correctly weighting the different
-        energies and theta values in an energy bin with an assumed
-        spectrum (defaulting to a powerlaw index 2) and then fitting a
-        single king function to this psf, which is considered the energy
-        bin average of the psf and should be a reasonable representation
-        of the width of the source, for the sake of fitting the extension
-        of a source.
-
-        N.B. For the newstyle psf which is represtened by the sum of
-        two king functions, it is best to weight each king function
-        independently and end up with an average core and average tail
-        PSF for the particular energy bin.  """
-
-    def __init__(self,bands,quiet):
-        """ Pass in the roi.bands object. """
-        self.bands=bands
-        self.quiet=quiet
-
-    def fit(self,weightspectrum=PowerLaw()):
-        """ Fit the psf. the fit values will be stored inside of
-            each band in the variables band.fit_gamma and band.fit_sigma,
-            or for the newstyle psf as band.fit_gamma_core
-            and band.fit_sigma_core & band.fit_gamma_tail and
-            band.fit_sigma_tail. """
-
-        if not self.quiet: 
-            print 'Fitting PSF to weighted average'
-        for band in self.bands: 
-            self._fit_band(band,weightspectrum)
-
-    def psf_base(self,g,s,delta):
-        # Fancy vectorized code taken from BandCALDBPsf. Maybe one
-        # day these functions can share a common base.
-        u = 0.5 * np.outer(delta,1./s)**2
-        y = (1-1./g)*(1+u/g)**(-g)/(2*np.pi*s**2)
-        return y
-
-    def _fit_band(self,band,weightspectrum):
-        """ Fit a psf with a default spectral index of 2. """
-
-        psf=band.sa.psf
-
-        newstyle = psf.newstyle
-
-        # Get a rough estimate of the gamma & sigma
-        # in the middle by weighting sigmas & gammas in the 
-        # geomeric mean energy over cos theta.
-        # This serves as the starting fit value.
-        scale = psf.scale_func[band.ct](band.e)
-        if newstyle:
-            nc,nt,gc,gt,sc,st,w = psf.get_p(band.e,band.ct)
-            gamma_middle=sum(w*(nc*gc+nt*gt))
-            sigma_middle=sum(w*(nc*sc+nt*st))
-        else:
-            g,s,w=psf.get_p(band.e,band.ct)
-            gamma_middle=sum(g*w)
-            sigma_middle=sum(s*w)
-
-        sigma_middle=sigma_middle*scale
-
-        rlist = np.linspace(0,20*sigma_middle,10000)
-        pdf = np.zeros_like(rlist)
-        pdf_weight = np.zeros_like(rlist)
-
-        weight_sum,s_list=0,[]
-
-        # Use the same simpson integral used to calculate
-        # predicted counts for a point source.
-        for e,sp in zip(band.sp_points,band.sp_vector):
-            scale = psf.scale_func[band.ct](e)
-            if newstyle:
-                nc,nt,gc,gt,sc,st,w = psf.get_p(e,band.ct)
-                sc *= scale
-                st *= scale
-                pdf += (w*(nc*self.psf_base(gc,sc,rlist)+
-                           nt*self.psf_base(gt,st,rlist))).sum(axis=1)*sp*weightspectrum(e)
-                s_list += list(sc) + list(st)
-            else:
-                g,s,w = psf.get_p(e,band.ct).copy()
-                s *= scale 
-                pdf += (w*self.psf_base(g,s,rlist)).sum(axis=1)*sp*weightspectrum(e)
-                s_list += list(s)
-
-            weight_sum += sp*weightspectrum(e)
-        
-        # Normalize
-        pdf /= weight_sum 
-
-        # Function returns the difference between the true pdf and 
-        # the psf for a given gamma & sigma. Note that the most
-        # accurate thing is the probability per unit radius summed over
-        # all of the radii.
-        f=lambda p: np.std(rlist*(self.psf_base(p[0],p[1],rlist).sum(axis=1)-pdf))
-
-        fit = fmin(f,[gamma_middle,sigma_middle],full_output=False,disp=False)
-        fit_gamma,fit_sigma = fit
-
-        # the fit sigma should be somewhere between the two extremes.
-        # Not so sure about the gamma tail parameter being a good
-        # measure.
-        if fit_sigma < min(s_list) or fit_sigma > max(s_list): 
-            if not self.quiet:
-                print dedent("""\
-                    Error: Sigma fit outside of a 
-                    reasonable range. fit sigma is %g,
-                    minimum sigma is %g. maximum sigma
-                    is %g.""" % \
-                    (fit_sigma,min(s_list),max(s_list)))
-
-        band.fit_gamma,band.fit_sigma=fit_gamma,fit_sigma
-
 
 class BandFitExtended(object):
 
