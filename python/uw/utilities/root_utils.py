ba9f08feb8cd1f6c0678c931964704f047b6ddaf
parent <>
2011-08-03 19:01:26 +0000

    *** empty log message ***

--- python/uw/utilities/root_utils.py
+++ python/uw/utilities/root_utils.py
@@ -5,20 +5,20 @@ Author: Damien Parent <dmnparent@gmail.com>
 """
 
 import numpy as np
-from ROOT import gROOT
+from ROOT import gROOT, gStyle, Double, kBlack
 # force the batch mode
 gROOT.SetBatch(True)
-from ROOT import gStyle    
 
-FONT = 42
+default_font = 83
 
-def initialization(batch=True):
+def initialization(batch=True, font=default_font):
     '''ROOT initialization.
 
     parameters:
     -----------
     batch : Run ROOT in batch mode [True/False]
     '''
+    
     print "Initializing ROOT ..."
 
     # general
@@ -30,9 +30,10 @@ def initialization(batch=True):
     gStyle.SetFillColor(0)
     gStyle.SetCanvasColor(10)
     gStyle.SetLineWidth(1)
-    gStyle.SetPalette(1)
-    gStyle.SetTextFont(FONT)
-
+    gStyle.SetPalette(8)
+    gStyle.SetTextFont(font)
+    #gStyle.SetTextSize(30)
+    
     # Frame
     gStyle.SetFrameBorderMode(0)
     gStyle.SetFrameFillColor(0)
@@ -40,24 +41,22 @@ def initialization(batch=True):
     # Pad
     gStyle.SetPadBorderMode(0)
     gStyle.SetPadColor(0)
-    gStyle.SetPadTopMargin(0.)
-    gStyle.SetPadLeftMargin(0.10)
-    gStyle.SetPadRightMargin(0.02)
-    gStyle.SetPadBottomMargin(0.)
+    gStyle.SetPadBottomMargin(0.1)
+    gStyle.SetPadTopMargin(0.01)
+    gStyle.SetPadLeftMargin(0.1)
+    gStyle.SetPadRightMargin(0.01)
     gStyle.SetPadTickX(1)    # make ticks be on all 4 sides.
     gStyle.SetPadTickY(1)
     gStyle.SetPadGridX(0)
     gStyle.SetPadGridY(0)
-    
+
     # histogram
     gStyle.SetHistFillStyle(0)
     gStyle.SetOptTitle(0)
-    
-    # histogram title
     gStyle.SetTitleSize(0.22)
-    gStyle.SetTitleFontSize(2)
-    gStyle.SetTitleFont(FONT)
-    gStyle.SetTitleFont(FONT,"xyz")
+    gStyle.SetTitleFontSize(10)
+    gStyle.SetTitleFont(font)
+    gStyle.SetTitleFont(font,"xyz")
     gStyle.SetTitleYOffset(1.0)
     gStyle.SetTitleXOffset(1.0)
     gStyle.SetTitleXSize(0.04)
@@ -69,41 +68,82 @@ def initialization(batch=True):
     
     # statistics box
     gStyle.SetOptStat(0)
-    gStyle.SetStatFont(FONT)
+    gStyle.SetStatFont(font)
+    gStyle.SetStatFontSize(10)
     gStyle.SetStatX(.91)
     gStyle.SetStatY(.90)
     gStyle.SetStatW(.15)
     gStyle.SetStatH(.15)
         
     # axis labels
-    gStyle.SetLabelFont(FONT)
-    gStyle.SetLabelFont(FONT,"xyz")
-    gStyle.SetLabelSize(0.035,"xyz")
+    gStyle.SetLabelFont(font)
+    gStyle.SetLabelFont(font,"xyz")
+    gStyle.SetLabelSize(10,"xyz")
     # gStyle.SetGridColor(1)
     gStyle.SetLegendBorderSize(1);
 
-
-def set_axis_thx( hist, x_title = "", x_title_size = 0., x_title_offset = 0., x_label_size = 0., 
-                  y_title = "", y_title_size = 0., y_title_offset = 0., y_label_size = 0. ):
     
+def set_axis_thx( hist, x_title = "", x_title_size = 0., x_title_offset = 0., x_label_size = 0., 
+                  y_title = "", y_title_size = 0., y_title_offset = 0., y_label_size = 0.,
+                  font=default_font, color=kBlack ):
+    
+    hist.SetLineWidth(1)
+    hist.SetLineColor(color)
+
     hist.GetXaxis().SetTitle(x_title)
     hist.GetXaxis().CenterTitle()
     hist.GetXaxis().SetTitleSize(x_title_size)
     hist.GetXaxis().SetTitleOffset(x_title_offset)
-    hist.GetXaxis().SetTitleFont(FONT)
+    hist.GetXaxis().SetTitleFont(font)
     hist.GetXaxis().SetLabelSize(x_label_size)
-    hist.GetXaxis().SetLabelOffset(0.01)
-    hist.GetXaxis().SetLabelFont(FONT)
+    hist.GetXaxis().SetLabelOffset(0.)
+    hist.GetXaxis().SetLabelFont(font)
+    hist.GetXaxis().SetNdivisions(510);
     
     hist.GetYaxis().SetTitle(y_title)
     hist.GetYaxis().CenterTitle()
     hist.GetYaxis().SetTitleSize(y_title_size)
     hist.GetYaxis().SetTitleOffset(y_title_offset)
-    hist.GetYaxis().SetTitleFont(FONT)
+    hist.GetYaxis().SetTitleFont(font)
     hist.GetYaxis().SetLabelSize(y_label_size)
     hist.GetYaxis().SetLabelOffset(0.01)
-    hist.GetYaxis().SetLabelFont(FONT)
+    hist.GetYaxis().SetLabelFont(font)
+    hist.GetYaxis().SetNdivisions(505);
+
+def set_axis( axis, title = "", title_size = 0., title_offset = 0., label_size = 0., font=default_font, div=510 ):
+    axis.SetTitle(title)
+    axis.CenterTitle()
+    axis.SetTitleSize(title_size)
+    axis.SetTitleOffset(title_offset)
+    axis.SetTitleFont(font)
+    axis.SetLabelSize(label_size)
+    axis.SetLabelOffset(0.01)
+    axis.SetLabelFont(font)
+    axis.SetNdivisions(div);
 
 def fit_lorentzian(x, par):
     return (0.5*par[0]*par[1]/np.pi) / np.max( 1.e-10,(x[0]-par[2]) * (x[0]-par[2]) + .25*par[1]*par[1])
 
+def ScaleGraphY(graph, factor):
+    '''Scales the y values of a graph with the factor in the argument'''
+    for i in range(graph.GetN()):
+        x, y = Double(0), Double(0)
+        graph.GetPoint(i,x,y)
+        y *= factor
+        graph.SetPoint(i,x,y)
+
+def zero_suppress(histo):
+    '''Zoom on the histogram'''
+    min_histo = histo.GetBinContent(histo.GetMinimumBin())
+    if min_histo != 0:
+        factor = 0.8 - np.sqrt(min_histo)/min_histo
+        min_level = int(min_histo*factor)
+        if min_level%5 == 0 or min_level%5 == 4: min_level += 3
+    else: min_level = 0.01
+    histo.SetMinimum(min_level)
+    
+def get_txtlevel(histo, factor):
+    min = histo.GetMinimum()
+    max = histo.GetMaximum()
+    return (max-min) * factor + min
+                               
