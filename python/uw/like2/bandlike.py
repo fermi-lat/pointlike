a86f84b285208abbfd51d0aefaec75eb1c057b8b
burnett <>
2011-09-05 18:54:29 +0000

    convert all model references to spectral_model

--- python/uw/like2/bandlike.py
+++ python/uw/like2/bandlike.py
@@ -11,7 +11,7 @@ classes:
 functions:
     factory -- create a list of BandLike objects from bands and sources
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/bandlike.py,v 1.1 2011/09/02 16:30:43 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/bandlike.py,v 1.2 2011/09/03 20:28:27 burnett Exp $
 Author: T.Burnett <tburnett@uw.edu> (based on pioneering work by M. Kerr)
 """
 
@@ -24,14 +24,14 @@ class BandSource(object):
 
     def __str__(self):
         return '%s for source %s (%s), band (%.0f-%.0f, %s)' \
-            % (self.__class__.__name__, self.source.name, self.model.name, self.band.emin, self.band.emax, 
+            % (self.__class__.__name__, self.source.name, self.source.spectral_model.name, self.band.emin, self.band.emax, 
                 ('front back'.split()[self.band.b.event_class()]),)
 
     def dump(self, out=None):
         """ useful summary dump """
         b = self.band
         print >>out, self.__str__()
-        if hasattr(self,'er'):
+        if hasattr(self,'exposure_ratio'):
             print >>out, '\texposure ratio, overlap: %.3f %.3f'%( self.exposure_ratio, self.overlap)
         pc = self.pix_counts
         print >>out, ('\tpixel counts: min, max, sum: '+3*'%8.1f') % ( pc.min(), pc.max(), pc.sum())
@@ -39,7 +39,7 @@ class BandSource(object):
 
     # maybe useful: not done yet
     #def norm(self):
-    #    return self.model[0]
+    #    return self.source.model[0]
     #    
     #def norm_error(self):
     #    # this is the uncertainty derived by taking the second derivative of the log likelihood
@@ -76,8 +76,6 @@ class BandDiffuse(BandSource):
         
         self.source_index = source.manager_index 
         self.source = source.factory(self.source_index) 
-        self.model  = self.source.smodel
-        self.source.quiet = True
         self.band = band
         self.initialize()
         self.update()
@@ -88,6 +86,7 @@ class BandDiffuse(BandSource):
             the model on a series of energy subplanes and storing the
             results for a later Simpson's rule.
         """
+        self.source.quiet = True
         self.source.initialize_counts([self.band])
         
     def update(self, fixed=False):
@@ -105,13 +104,14 @@ class BandDiffuse(BandSource):
     def grad(self, weights, phase_factor=1):
         """ contribution to the gradient
         """
-        nfree =np.sum(self.model.free)
+        model = self.source.smodel # beware that this kind of source has a 'smodel'
+        nfree =np.sum(model.free)
         if nfree==0: return []
-        if (nfree==1) and self.model.free[0]:
+        if (nfree==1) and model.free[0]:
             # special case -- only normalization free 
             apterm  =  phase_factor*self.counts
             pixterm =  (self.pix_counts*weights).sum() 
-            g = [(apterm - pixterm)/self.model.getp(0)]
+            g = [(apterm - pixterm)/model.getp(0)]
         else:
             # this is only rarely needed and not carefully tested
             self.band.pix_weights = weights
@@ -129,7 +129,8 @@ class BandExtended(BandDiffuse):
         this is a subclass, but allowing for separate implemetation of its details
     """
     def initialize(self):
-        self.source.initialize_counts([self.band]) 
+        #was: self.source.initialize_counts([self.band]) 
+        super(BandExtended, self).initialize()
         myband = self.source.bands[0]
         self.exposure_ratio, self.overlap = myband.er, myband.overlaps # for info for now
 
@@ -137,7 +138,8 @@ class BandExtended(BandDiffuse):
         """ contribution to the gradient
         """
         #  should be exactly the same code as for BandPoint -- try when have a variable extended source
-        if np.sum(self.model.free)==0 : return []
+        model = self.source.smodel # beware that this kind of source has a 'smodel'
+        if np.sum(model.free)==0 : return []
         self.band.pix_weights = weights
         self.band.phase_factor = phase_factor
         return self.source.gradient([self.band], self.source_index)
@@ -153,7 +155,6 @@ class BandPoint(BandSource):
                 
         """
         self.source, self.roi_dir = source.factory(source.manager_index) 
-        self.model = self.source.model
         self.band = band
         self.initialize()
         self.update()
@@ -174,7 +175,7 @@ class BandPoint(BandSource):
             pixel counts and total counts in ROI
         """
         b = self.band
-        expected = b.exposure_integral(self.model) * self.exposure_ratio
+        expected = b.exposure_integral(self.source.spectral_model) * self.exposure_ratio
         self.pix_counts = self.psf_pixel_values * expected
         self.counts = expected*self.overlap
 
@@ -182,7 +183,7 @@ class BandPoint(BandSource):
         """ contribution to the overall gradient
         """
         b = self.band
-        model = self.model
+        model = self.source.spectral_model
         if np.sum(model.free)==0 : return []
         # Calculate the gradient of a spectral model (wrt its parameters) integrated over the exposure.
         g = b.exposure_integral(model.gradient, axis=1)[model.free] * self.exposure_ratio
