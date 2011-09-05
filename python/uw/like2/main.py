9e84c0d7fa577d31c93f09b82b0c52c4eac54be4
burnett <>
2011-09-05 18:59:27 +0000

    add TS function, quick only for now add source_flux function to return SourceFlux object to manage the SED for a source

--- python/uw/like2/main.py
+++ python/uw/like2/main.py
@@ -1,7 +1,7 @@
 """
 Top-level code for ROI analysis
 
-$Header$
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/main.py,v 1.1 2011/09/02 16:30:43 burnett Exp $
 
 """
 
@@ -37,29 +37,35 @@ class Main(roistat.ROIstat):
         mm(**fit_kw)
         print '%d calls, likelihood improvement: %.1f'\
             % (self.calls, self.log_like() - initial_value)
-        if fit_kw['estimate_errors'] and select is not None:
+        if fit_kw['estimate_errors'] and select is None:
             # tricky to do if fitting subset, punt for now
             self.sources.set_covariance_matrix(mm.cov_matrix)
         return mm
         
-    def localize(self, **kwargs):
-        return tools.Localize(self, **kwargs)
+    def localize(self, source_name, **kwargs):
+        """ for now, return a Localization object, which see """
+        return tools.Localization(self, source_name, **kwargs)
         
-    def pardump(self, eps=1e-1):
-        print 'variable parameters'
-        par = self.get_parameters()
-        grad = self.gradient(par)
-        f0 = self(par)
-        for i, name in enumerate(self.parameter_names):
-            dpar = par.copy()
-            dpar[i] += eps/grad[i]
-            print '%-20s%10.2e%10.2e %10.2e'% (name, par[i], grad[i], (self(dpar)-f0)*grad[i]/eps)
-            self.set_parameters(par)
-            
+           
     def summary(self, out=None):
         """ summary table of free parameters"""
         print >>out, '%-20s%10s'% tuple('Name value'.split())
         for (name, value) in zip(self.parameter_names, self.parameters):
             print >>out, '%-20s %10.4g' %(name,value)
             
-    
+    def TS(self, source_name, quick=True):
+        """ measure the TS for the given source
+        """
+        source = self.sources.find_source(source_name)
+        model = source.spectral_model
+        norm =model[0]
+        model[0]=1e-15
+        self.update()
+        llzero = self.log_like()
+        model[0]=norm; self.update()
+        ts= 2*(self.log_like()-llzero)
+        return ts
+
+    def source_flux(self, source_name, **kwargs):
+        """ return a SourceFlux object for the source, which see"""
+        return tools.SourceFlux(self, source_name, **kwargs)
