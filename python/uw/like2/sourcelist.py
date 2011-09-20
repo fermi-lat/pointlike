8f30373782af520a479407df82783d350881a50e
burnett <>
2011-09-20 15:54:37 +0000

    change to initialize from object containing bgmodels and point_sources (not ROIAnalysis anymore)

--- python/uw/like2/sourcelist.py
+++ python/uw/like2/sourcelist.py
@@ -1,7 +1,7 @@
 """
 Manage sources: single class SourceList
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/sourcelist.py,v 1.5 2011/09/05 18:53:09 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/sourcelist.py,v 1.6 2011/09/18 17:43:56 burnett Exp $
 Author: T.Burnett <tburnett@uw.edu>
 """
 
@@ -16,10 +16,10 @@ class SourceList(list):
         """ initialization: get sources from the ROIAnalysis object: creates a polymorphic list of the 
             three types: point, global, and extended
             
-            roi : a ROIAnalsis object:
+            roi : an ROIdef object:
                 expect to find attributes:
-                    dsm : a ROIDiffuseManager object, for the list of global and extended sources
-                    psm : a ROIPointSourceManager object, with a list of point sources
+                    bgmodels : a list of global and extended sources
+                    point_sources : list of point sources
                     roi_dir: the center of the ROI
         """
         class DiffuseSourceFactory(object):
@@ -27,32 +27,33 @@ class SourceList(list):
             """
             def __init__(self, roi):   self.roi = roi
             def __call__(self, source_index, **kwargs):
-                cm = self.roi.dsm.bgmodels[source_index]
+                cm = self.roi.bgmodels[source_index]
                 src = cm.__class__(cm.sa, cm.diffuse_source, self.roi.roi_dir, **kwargs)
                 src.spectral_model = src.smodel
                 return src
         class ExtendedSourceFactory(object):
             def __init__(self, roi):   self.roi = roi
             def __call__(self, source_index, **kwargs):
-                cm = self.roi.dsm.bgmodels[source_index]
+                cm = self.roi.bgmodels[source_index]
                 src = cm.__class__(cm.sa, cm.extended_source, self.roi.roi_dir, **kwargs)
                 src.spectral_model = src.smodel
                 return src
         class PointSourceFactory(object):
             def __init__(self, roi):    self.roi =roi
             def __call__(self, source_index):
-                return roi.psm.point_sources[source_index], self.roi.roi_dir
+                return roi.point_sources[source_index], self.roi.roi_dir
                 
         # note that sources are added in the order diffuse, point to agree with ROIAnalysis
-        # also, add two attributes to ecach source object and define 'spectral_model'
-        for i,source in enumerate(roi.dsm.diffuse_sources):
-            spatialclassname = roi.dsm.bgmodels[i].__class__.__name__
+        # also, add two attributes to each source object and define 'spectral_model'
+        for i, bgmodel in enumerate(roi.bgmodels):
+            spatialclassname = bgmodel.__class__.__name__
+            source = bgmodel.diffuse_source
             source.factory = ExtendedSourceFactory(roi) if spatialclassname=='ROIExtendedModel'\
                 else DiffuseSourceFactory(roi)
             source.manager_index = i
             source.spectral_model = source.smodel
             self.append(source)
-        for i,source in enumerate(roi.psm.point_sources):
+        for i,source in enumerate(roi.point_sources):
             source.manager_index = i
             source.factory = PointSourceFactory(roi)
             source.spectral_model = source.model
@@ -64,7 +65,7 @@ class SourceList(list):
         ## temporary kluge to ensure that bands are setup for current use by diffuse
         ## this is needed if the ROI is generated without its setup, which adds these
         ## attributes to the bands
-        nm  = len(roi.dsm.bgmodels)
+        nm  = len(roi.bgmodels)
         for band in roi.bands:
             band.bg_counts = np.empty(nm)
             band.bg_pix_counts = np.empty([len(band.wsdl),nm]) if band.has_pixels else 0
