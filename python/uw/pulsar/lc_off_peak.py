71ce2d3f0dd3e885753527960c9b81bf243f3bdb
lande <>
2011-10-03 04:39:36 +0000

    improve second off pulse calculation

--- python/uw/pulsar/lc_off_peak.py
+++ python/uw/pulsar/lc_off_peak.py
@@ -3,19 +3,25 @@ This module implements the class OffPeak that
 can be used to calculate the off-peak region
 of a pulsar.
 
-$Header: $
+$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/pulsar/lc_off_peak.py,v 1.1 2011/10/02 23:37:22 lande Exp $
 
 author: J. Lande <joshualande@gmail.com>
 
 """
-import sympy
 import numbers
+
+import sympy
+import numpy as np
+from scipy.optimize import fmin, brute
+from scipy.stats import poisson
+
         
+from uw.utilities import keyword_options
 from . phase_range import PhaseRange
 
 
 class OffPeak(object):
-    """ Object that can be used to define the off pulse 
+    """ Object that can be used to define the off peak 
         window for a pulsar.
 
         This object requires an lcfitters object.
@@ -29,71 +35,146 @@ class OffPeak(object):
         but so is the 1% contaminuation criteria in the first place.
     """
 
-    contamination = 0.01 # fraction of pulsar allowed in off peak
+    defaults = (
+        ("contamination", 0.01,"fraction of pulsar allowed in off peak"),
+        ("quiet",False,"Don't print out"),
+    )
 
-    def __init__(self,lct):
-        self.lct = lct # uw.pulsar.lcfitters.LCTemplate
+    @keyword_options.decorate(defaults)
+    def __init__(self, lcf, **kwargs):
+        keyword_options.process(self, kwargs)
+
+        self.lcf = lcf # uw.pulsar.lcfitters.LCFitter
+
+        self.lct = lcf.template # uw.pulsar.lcfitters.LCTemplate
 
         self._fit()
 
-    def _find_range(center):
+    @staticmethod
+    def get_counts(phase_range,phases):
+        """ Return the number of photons in the given phase range. """
+        return sum(phase in phase_range for phase in phases)
+
+    def _find_range(self, center):
         c = center
         lct = self.lct
 
+        # Note, by definition lct.integrate(0, 1) = 1
 
-
-        integral=lambda dphi: lct.integrate(c-dphi/2, c+dphi/2) - self.lowest_value*(dphi)
-        total_integral = lct.integrate(0, 1) - self.lowest_value
+        def integral(dphi):
+            l=self.lowest_value
+            return (lct.integrate(c-dphi/2, c+dphi/2) - l*(dphi))/(1-l)
  
+        # get a better starting guess
         dphi_guess=0
-        while integral(dphi_guess)/total_integral < OffPeak.contamination:
+        while integral(dphi_guess) < self.contamination:
             dphi_guess += .01
 
-        residual=lambda dphi: np.abs(integral(dphi)/total_integral - OffPeak.contamination)
+        residual=lambda dphi: np.abs(integral(dphi) - self.contamination)
 
         # N.B., the integral over all phase is equal to 1
         dphi=fmin(residual, dphi_guess, disp=False)[0]
 
-        int = integral(dphi)/total_integral
-        print ' center = %s, dphi = %s, int=%s' % (center,dphi,int)
+        if not self.quiet: 
+            print ' center = %s, dphi = %s (int=%s)' % (center,dphi,integral(dphi))
 
         return PhaseRange(center-dphi/2,center+dphi/2)
 
+    @staticmethod
+    def peak_location(primitive):
+        return primitive.p[np.asarray(primitive.pnames) == 'Location'][0]
+
+    @staticmethod
+    def peak_locations(lct):
+        return [OffPeak.peak_location(p) for p in lct.primitives]
+
+    @staticmethod
+    def peaks_between(a,b,peaks):
+        """ Calculate number of peaks between a and b
+            where peaks is a list of peaks. """
+
+        # make sure a,b are from 0-1 and b>a
+        a,b=a%1,b%1
+        a,b=min(a,b),max(a,b)
+        if b-a < (a-0) + (1-b):
+            range = PhaseRange(a,b)
+        else:
+            range = PhaseRange(b,a)
+        return sum(peak in range for peak in peaks)
+
     def _fit(self):
+
         lct = self.lct
+        lcf = self.lcf
 
         # (*) Calulate the lowest part of the light curve using a simple
         #     grid search.
 
-        lowest_phase, lowest_value, grid, jout = scipy.optimize.brute(lct, (0,1), ns=1000)
-        self.lowest_value = lowest_value
+        grid = (0,1, .001)
+        lowest_phase = brute(lct, (grid,))
+        self.lowest_value = lct(lowest_phase)
 
         # (*) Calculate the phase range for a grid of centers in phase.
 
         centers=np.linspace(0,1,101)
-        ranges = np.asarray([self._find_range(c) for c in np.linspace(0,1,101)])
+        ranges = [self._find_range(c) for c in centers]
+        dphis = np.asarray([range.phase_fraction for range in ranges])
         
         i = np.argmax(dphis)
-        self.off_pulse = ranges[i]
+        self.first_off_peak = ranges[i]
+        self.first_center = centers[i]
+        self.first_dphi = centers[i]
 
-        # (*) Find the second largest region that does not overlap the first
-        #     region.
+        if not self.quiet:
+            print 'Best Off Peak region is ',self.first_off_peak
 
-        overlap = np.asarray([])
-        j = np.argmax(dphis[ overlap == False ])
-        second_off_pulse = [ min_phase[ overlap == False ][j], max_phase[ overlap == False ][j] ]
+        # (*) Find the largest good alternate region
 
-        # (*) Test if regions are statistically consistent with eachother
+        peaks = OffPeak.peak_locations(lct)
+        peaks_between = np.asarray([OffPeak.peaks_between(self.first_center,c,peaks) for c in centers])
+        overlaps = np.asarray([self.first_off_peak.overlaps(r) for r in ranges])
 
-        self.ncounts1 = self.get_counts()
-        self.ncounts2 = self.get_counts()
+        good_region = (~overlaps) & \
+                (peaks_between > 0) & \
+                (len(peaks)>1)
+#                (dphis > self.first_dphi/8.0) & \
 
-        if regions_are_consistent:
-            self.off_pulse += second_off_pulse
+        if len(peaks) < 2:
+            print 'No good off pulse regions'
+            self.off_peak = self.first_off_peak
+            return
 
-    @property
-    def off_pulse(self):
-        return self.off_pulse
+        i = np.argmax(np.where(~good_region,-np.inf,dphis))
+        self.second_off_peak = ranges[i]
 
+        if not self.quiet:
+            print 'Second off pulse region candidate is', self.second_off_peak
+
+        # (*) Test if second regions are statistically consistent with eachother
+
+        counts = self.get_counts(self.first_off_peak,lcf.phases)
+
+        # (*) Calculate total counts in the second region
+
+        print 'Note, this may not be the optimal way to calculate number of counts (at least, when pulsar weighting...!'
+        second_counts = self.get_counts(self.second_off_peak, lcf.phases)
+
+        # counts predicted in second region if no pulsed emission!
+        prediced_counts = counts*(self.second_off_peak.phase_fraction/self.first_off_peak.phase_fraction)
+
+        poisson_likelihood = poisson.sf(second_counts,prediced_counts)
+
+        if not self.quiet:
+            print 'Poisson likelihood for second region is %.3f' % poisson_likelihood
+
+        # keep second region if probability of obtaining more counts than observed
+        # by the second region is > 5% (the second region is not unusually
+        # large).
+        if poisson_likelihood > 0.05:
+            if not self.quiet: print 'Keeping second region'
+            self.off_peak = self.first_off_peak + self.second_off_peak
+        else:
+            if not self.quiet: print 'Rejecting second region'
+            self.off_peak = self.first_off_peak
 
 
