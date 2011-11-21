95f390a4cf304396dbebd1852f29b4d737c3e702
burnett <>
2011-11-21 14:33:14 +0000

    tweak to remove some like dependence

--- python/uw/like2/roisetup.py
+++ python/uw/like2/roisetup.py
@@ -1,14 +1,38 @@
 """
 Set up an ROI 
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/roisetup.py,v 1.2 2011/10/01 13:35:06 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/roisetup.py,v 1.5 2011/11/16 14:14:20 burnett Exp $
 
 """
 import os
 import numpy as np
+import skymaps
 from . import dataset, skymodel, diffuse
 from .. utilities import keyword_options, convolution
-from .. like import roi_extended
+from .. like import roi_extended, pypsf
+
+        
+class ExposureManager(object):
+    """A small class to handle the trivial combination of effective area and livetime."""
+
+    def __init__(self, sa):
+
+        skymaps.EffectiveArea.set_CALDB(sa.CALDBManager.CALDB)
+        skymaps.Exposure.set_cutoff(np.cos(np.radians(sa.thetacut)))
+        inst = ['front', 'back']
+        aeff_files = sa.CALDBManager.get_aeff()
+        ok = [os.path.exists(file) for file in aeff_files]
+        if not all(ok):
+            raise DataSetError('one of CALDB aeff files not found: %s' %aeff_files)
+        self.ea  = [skymaps.EffectiveArea('', file) for file in aeff_files]
+        if sa.verbose: print ' -->effective areas at 1 GeV: ', ['%s: %6.1f'% (inst[i],self.ea[i](1000)) for i in range(len(inst))]
+        if sa.use_weighted_livetime:
+            self.exposure = [skymaps.Exposure(sa.lt,sa.weighted_lt,ea) for ea in self.ea]
+        else:
+            self.exposure = [skymaps.Exposure(sa.lt,ea) for ea in self.ea]
+
+    def value(self, sdir, energy, event_class):
+        return self.exposure[event_class].value(sdir, energy)
 
 
 class ROIfactory(object):
@@ -17,7 +41,7 @@ class ROIfactory(object):
     
     """
     defaults =(
-        ('analysis_kw', dict(irf='P7SOURCE_V6',minROI=7,maxROI=7, emax=316277),'roi analysis'),
+        ('analysis_kw', dict(irf='P7SOURCE_V6',minROI=7,maxROI=7, emax=316277, quiet=False),'roi analysis'),
         ('skymodel_kw', {}, 'skymodel keywords'),
         ('convolve_kw', dict( resolution=0.125, # applied to OTF convolution: if zero, skip convolution
                             pixelsize=0.05, # ExtendedSourceConvolution
@@ -32,15 +56,18 @@ class ROIfactory(object):
         """ 
         parameters
         ----------
+        indir: folder containing skymodel definition
         dataname : string
                 used to look up data specification
-            indir: folder containing skymodel definition
         """
         keyword_options.process(self, kwargs)
         self.skymodel = skymodel.SkyModel(indir,  **self.skymodel_kw)
         self.dataset = dataset.DataSet(dataname, **self.analysis_kw)
-        self.exposure = self.dataset.exposure 
-        self.psf  = self.dataset.psf
+        self.exposure  = ExposureManager(self.dataset)
+        self.psf = pypsf.CALDBPsf(self.dataset.CALDBManager)
+ 
+        #self.exposure = self.dataset.exposure 
+        #self.psf  = self.dataset.psf
         convolution.AnalyticConvolution.set_points(self.convolve_kw['num_points'])
         convolution.ExtendedSourceConvolution.set_pixelsize(self.convolve_kw['pixelsize'])
 
@@ -91,7 +118,7 @@ class ROIfactory(object):
         global_sources, extended_sources = self._diffuse_sources(src_sel)
         return ROIdef( name=src_sel.name() ,
                     roi_dir=skydir, 
-                    bands=self.dataset(skydir), 
+                    bands=self.dataset(self.psf, self.exposure, skydir), 
                     global_sources=global_sources,
                     extended_sources = extended_sources,
                     point_sources=self._local_sources(src_sel), **roi_kw)
