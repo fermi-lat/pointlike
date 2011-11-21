7fdacb594998fb9ef6655357113d9e32caad077f
burnett <>
2011-11-21 14:34:06 +0000

    big change to use new data specification

--- python/uw/like2/dataset.py
+++ python/uw/like2/dataset.py
@@ -1,41 +1,60 @@
 """  
  Setup the ROIband objects for an ROI
  
-    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/dataset.py,v 1.5 2011/10/20 21:41:27 burnett Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/dataset.py,v 1.6 2011/11/02 19:17:06 burnett Exp $
 
     authors: T Burnett, M Kerr, J. Lande
 """
-version='$Revision: 1.5 $'.split()[1]
+version='$Revision: 1.6 $'.split()[1]
 import os, glob, types
 import numpy as np
-from uw.like import pixeldata, pypsf, pycaldb, pointspec_helpers, roi_bands
-from uw.utilities import keyword_options
-from .. pipeline import dataspec
+import skymaps
+from ..like import pycaldb, roi_bands
+from ..data import dataman
 
-class DataSet(object):
+from ..utilities import keyword_options
+
+class DataSetError(Exception):pass
+
+class DataSpecification(object):
     """ 
-    Manage the data, producing a set of ROIBand objects with __call__
     """
+    def __init__(self, folder, **data):
+        """
+        folder : string
+            the path to the folder where the dictionary was found
+        data : dict
+            dictionary that contains entries sufficient to describe the data, and how to generate it
+        """
+        if folder=='.': folder = os.getcwd()
+        for key in 'ft1files ft2files binfile ltcube'.split():
+            if key in data and data[key] is not None:
+                data[key]=os.path.expandvars(data[key]) 
+                if not os.path.isabs(key):
+                    data[key] = os.path.join(folder,data[key])
+                # need a check, but will fail if need to glob
+                #assert os.path.exists(data[key]), 'DataSpec: file %s not found' % data[key]
+        self.__dict__.update(data)
+        #print 'data spec:\n', str(self.__dict__)
 
-    defaults = (
-        'keywords controlling data binning and livetime calculation',
-        ('exp_radius',20,'radius (deg) to use if calculate exposure or a ROI. (180 for full sky)'),
+    def __str__(self):
+        return self.data_name
+        
+
+        
+class DataSet(dataman.DataSpec):
+    """
+    subclass of data.dataman.DataSpec that uses dicts to specify data, 
+        and sets up instrument response and  ROI setup
+        
+    Note the __call__ function
+    """
+    defaults = dataman.DataSpec.defaults + (
+        ' legacy key words that should be in DSS',
         ('zenithcut',105,'Maximum spacecraft pointing angle with respect to zenith to allow'),
         ('thetacut',66.4,'Cut on photon incidence angle'),
-        ('event_class',3,'select class level (3 - diffuse; 2 - source; 1 - transient; 0 - Monte Carlo)'),
-        ('conv_type',-1,'select conversion type (0 - front; 1 - back; -1 = front + back)'),
-        ('tstart',0,'Default no cut on time; otherwise, cut on MET > tstart'),
-        ('tstop',0,'Default no cut on time; otherwise, cut on MET < tstop'),
-        ('recalcgti',False,'if True, try to get GTI from GT1 files; otherwise, try from livetime cube or binned data file'),
-        ('binsperdec',4,'energy binning granularity when binning FT1'),
-        ('emin',100,'Minimum energy'),
-        ('emax',1e6,'Maximum energy'),
         ('use_weighted_livetime',False,'Use the weighted livetime'),
-        
-        'keywords for monte carlo data',
-        ('mc_src_id',-1,'set to select on MC_SRC_ID column in FT1'),
-        ('mc_energy',False,'set True to use MC_ENERGY instead of ENERGY'),
-        
+
         'keywords controlling instrument response',
         ('irf',None,'Which IRF to use'),
         ('psf_irf',None,'specify a different IRF to use for the PSF; must be in same format/location as typical IRF file!'),
@@ -43,46 +62,39 @@ class DataSet(object):
         ('custom_irf_dir',None,'override the CUSTOM_IRF_DIR specified by the env. variable'),
         
         'keywords defining actual ROI setup',
+        ('emin',100,'Minimum energy for selected bands'),
+        ('emax',1e6,'Maximum energy for selected bands'),
         ('minROI', 7, 'minimum ROI radius'),
         ('maxROI', 7, 'maximum ROI radius'),
         ('phase_factor', 1.0, 'phase factor for pulsar phase analysis'),
-        
-        'miscelleneous',
-        ('quiet',False,'Set True to suppress (some) output'),
-        ('verbose',False,'More output'),
-    )
+
+        ('verbose', False, 'more output'),)
+    
     @keyword_options.decorate(defaults)
     def __init__(self, dataset_name, **kwargs):
         """
         Create a new DataSet object.
 
-        data_specification: an instance of DataSpecification with links to the FT1/FT2,
+        dataset_name: an instance of DataSpecification with links to the FT1/FT2,
                             and/or binned data / livetime cube needed for analysis
-                            (see docstring for that class) """
+                            (see docstring for that class) 
+        """
 
-        self.dataspec = self._process_dataset(dataset_name)
-
-        self.__dict__.update(self.dataspec.__dict__)
-        keyword_options.process(self, kwargs)
-        assert self.irf is not None, 'Must provide the name of an IRF to use'
-        self.CALDBManager = pycaldb.CALDBManager(irf=self.irf,psf_irf=self.psf_irf,
-            CALDB=self.CALDB,custom_irf_dir=self.custom_irf_dir)
-
-         #TODO -- sanity check that BinnedPhotonData agrees with analysis parameters
-        self.pixeldata = pixeldata.PixelData(self.__dict__)
-        self.exposure  = pointspec_helpers.ExposureManager(self)
-        self.psf = pypsf.CALDBPsf(self.CALDBManager)
-    
-    def __str__(self):
-        #s = 'Data selection, class %s\n' %self.__class__.__name__
-        show = """data_name irf minROI maxROI emin emax""".split()
-        d = {}
-        for key in show:
-            d[key] = self.__dict__.get(key, 'not in self.__dict__!')
-        return str(d)
-            #s += '\t%-20s: %s\n' %(key,
-            #    self.__dict__[key] if key in self.__dict__.keys() else 'not in self.__dict__!')
-        #return s
+        dataspec = self._process_dataset(dataset_name).__dict__
+        dataspec.update(
+                ft1=dataspec.pop('ft1files',None), 
+                ft2=dataspec.pop('ft2files',None),
+                binsperdec=4,
+                legacy=True,)
+        dataspec.update(kwargs)
+        super(DataSet,self).__init__( **dataspec)
+        self.CALDBManager = pycaldb.CALDBManager(
+                irf=self.irf, 
+                psf_irf=self.psf_irf,
+                CALDB=self.CALDB,
+                custom_irf_dir=self.custom_irf_dir)
+        self.lt = skymaps.LivetimeCube(self.ltcube,weighted=False) ###<< ok?
+        self._load_binfile()
 
     def _process_dataset(self,dataset,month=None):
         """ Parse the dataset as either a DataSpecification object, a dict, or a string lookup key.
@@ -103,36 +115,53 @@ class DataSet(object):
                 try:
                     ldict = eval(open(dict_file).read())
                 except:
-                    print 'Data dictionary file %s not valid' % ldict
-                    raise
+                    raise DataSetError( 'Data dictionary file %s not valid' % ldict)
                 if dataset in ldict: 
                     print 'found dataset %s in %s' % (dataset, folder)
-                    return dataspec.DataSpecification(folder, **ldict[dataset])
+                    return DataSpecification(folder, **ldict[dataset])
         # not found: this is deprecated, leave for backwards consisency
-        raise RuntimeError('dataset name %s not found in %s' % (dataset, folders))
-        return dataspec.DataSpec(dataset,month=month)
+        raise DataSetError('dataset name %s not found in %s' % (dataset, folders))
 
-    def __call__(self, roi_dir, **kwargs):
+    def __call__(self, psf, exposure, roi_dir, **kwargs):
         """ return array of ROIBand objects for given direction, radius, emin, emax
+        
+        Parameters
+        ----------
+        sa : object of a class
+            must have psf and 
         """
         self.bands = []
         band_kwargs= dict()
         band_kwargs.update(kwargs)
-        for band in self.pixeldata.dmap:
+        class ForBand(object):
+            """ mix stuff that ROIBand wants """
+            def __init__(self, minROI, maxROI):
+                self.__dict__.update(psf=psf, exposure=exposure, 
+                        minROI=minROI, maxROI=maxROI)
+        forband = ForBand(self.minROI,self.maxROI)
+        for band in self.dmap:
             if (band.emin() + 1) >= self.emin and (band.emax() - 1) < self.emax:
                 # note: pass self to ctor for minROI, maxROI, exposure, psf.band_psf
-                self.bands.append(roi_bands.ROIBand(band, self, roi_dir, **band_kwargs))
-
+                self.bands.append(roi_bands.ROIBand(band, forband, roi_dir, **band_kwargs))
         return np.asarray(self.bands)
 
-    def info(self):
-        self.pixeldata.dmap.info()
-        
+    def _load_binfile(self):
+        if not self.quiet: print 'loading binfile %s ...' % self.binfile ,
+        self.dmap = skymaps.BinnedPhotonData(self.binfile)
+        if not self.quiet: print 'found %d bands, energies %.0f-%.0f MeV'\
+                % (len(self.dmap), self.dmap[1].emin(), self.dmap[len(self.dmap)-1].emax())
+
+        if self.verbose:
+            self.datainfo()
+            print '---------------------'
+            
+    def datainfo(self):
+        self.dmap.info()
+
 
 def main(dataname='2years_z100', irf='P7SOURCE_V6',**kwargs):
     """ for testing: must define a dataset, and and at least an IRF"""
-    from uw.pipeline import dataspec
-    ds = dataspec.DataSpec(dataname)
+    ds = DataSet(dataname)
     analysis_kw = dict(irf=irf, minROI=7, maxROI=7)
     analysis_kw.update(kwargs)
     ds = DataSet(ds, **analysis_kw)
