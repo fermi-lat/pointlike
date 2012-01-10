79c419bd8913f982705353d643347ae6503a8e92
lande <>
2012-01-10 22:17:56 +0000

    better kernel normaliation

--- python/uw/like/roi_image.py
+++ python/uw/like/roi_image.py
@@ -6,7 +6,7 @@ the data, and the image.ZEA object for plotting.  The high level object
 roi_plotting.ROIDisplay can use to access these objects form a high
 level plotting interface.
 
-$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_image.py,v 1.35 2011/08/25 02:04:36 lande Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/like/roi_image.py,v 1.36 2011/09/20 04:38:01 lande Exp $
 
 author: Joshua Lande
 """
@@ -900,12 +900,12 @@ class SmoothedImage(ROIImage):
             kernel = np.zeros([kernelsize,kernelsize],dtype=float)
             xx,yy = np.indices(kernel.shape)
             rr = np.sqrt((xx-kernel.shape[0]/2.)**2+(yy-kernel.shape[1]/2.)**2)
-            kernel = np.exp(-(rr**2)/(2*width**2)) / (width**2 * (2*np.pi))
-
+            kernel = np.exp(-(rr**2)/(2*width**2))
 
         else:
             raise Exception("...")
 
+        kernel /= kernel.sum()
         return kernel
 
 
