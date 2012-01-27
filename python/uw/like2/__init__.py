4e439b8980c21294696fc3508446cff852ab0b08
burnett <>
2012-01-27 15:10:59 +0000

    change to simplify startup

--- python/uw/like2/__init__.py
+++ python/uw/like2/__init__.py
@@ -1,8 +1,30 @@
 """
-Package containing an alternative (to like) likelihood analysis view
+Package containing likelihood analysis
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/__init__.py,v 1.1 2011/08/18 16:27:03 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/__init__,v 1.2 2011/09/28 17:35:52 burnett Exp $
 Authors:  T. Burnett, M. Kerr, E. Wallace, M. Roth, J. Lande
 
+Modules in this package, and basic dependencies
+
+main 
+    roisetup 
+        diffuse 
+            models
+        dataset
+        skymodel
+            sources
+    roistat
+        sourcelist
+        bandlike
+    localization 
+        plotting.tsmaps
+    sedfuns
+    printing
+    
+contained packages
+    pipeline
+    plotting
+    pub
 """
 
+from .main import factory
