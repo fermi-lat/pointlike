c62603f6664f71f3857bd794e30c8efa54cb0753
kadrlica <>
2012-05-02 01:47:57 +0000

    Call cache when shrinking model

--- python/uw/darkmatter/spatial.py
+++ python/uw/darkmatter/spatial.py
@@ -1,13 +1,13 @@
 """A set dark matter spatial models for pointlike analyses
 
-    $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/darkmatter/spatial.py,v 1.2 2012/04/26 20:35:26 lande Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/darkmatter/spatial.py,v 1.3 2012/04/30 20:33:21 lande Exp $
 
     author: Joshua Lande, Alex Drlica-Wagner
 """
 import numpy as np
 import pickle
 from uw.like.SpatialModels import RadiallySymmetricModel, SMALL_ANALYTIC_EXTENSION, PseudoSpatialModel
-from uw.like.SpatialModels import InterpProfile2D, SpatialMap
+from uw.like.SpatialModels import InterpProfile2D, SpatialMap, smart_log
 
 # Degrees, should come from the file...
 
@@ -40,8 +40,8 @@ class NFW(InterpProfile2D):
 
     def _shrink(self,size=None): 
         if size is None: size = min(self.sigmas)
-        self.p[2]=smart_log(size,log=self.log[2])
-        self.free[2]=False
+        self.setp('Sigma',size)
+        self.freeze('Sigma')
 
     def can_shrink(self): return True
 
@@ -76,8 +76,8 @@ class Einasto(InterpProfile2D):
 
     def _shrink(self,size=None): 
         if size is None: size = min(self.sigmas)
-        self.p[2]=smart_log(size,log=self.log[2])
-        self.free[2]=False
+        self.setp('Sigma',smart_log(size,log=self.log[2]))
+        self.freeze('Sigma')
 
     def can_shrink(self): return True
 
@@ -112,8 +112,8 @@ class Burkert(InterpProfile2D):
 
     def _shrink(self,size=None): 
         if size is None: size = min(self.sigmas)
-        self.p[2]=smart_log(size,log=self.log[2])
-        self.free[2]=False
+        self.setp('Sigma',smart_log(size,log=self.log[2]))
+        self.freeze('Sigma')
 
     def can_shrink(self): return True
 
