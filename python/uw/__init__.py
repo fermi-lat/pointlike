735ae38b35fea074aa471574ee3e7587e718fef6
burnett <>
2012-06-24 14:16:00 +0000

    *** empty log message ***

--- python/uw/__init__.py
+++ python/uw/__init__.py
@@ -1,21 +1,6 @@
 """
 THe UW system for performing spectral analysis 
 
-at this top level define a simple "factory" function giving a simple user access to the system
-
-$Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/__init__.py,v 1.6 2010/06/21 19:52:32 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/__init__.py,v 1.7 2010/09/21 21:58:08 lande Exp $
 Authors:  T. Burnett, M. Kerr, E. Wallace, M. Roth, J. Lande
 """
-
-def factory(**kwargs):
-
-    from thb_roi import roi_factory
-    import os
-
-#    f = roi_factory.ROIfactory(config.AE(**kwargs))
-    f = roi_factory.ROIfactory(**kwargs)
-       
-    if os.name=='nt':
-        os.system('title %s' %os.getcwd())
-    return f
-    
