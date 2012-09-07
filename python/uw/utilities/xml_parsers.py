83a5803a4043908cefc456a48905e6fd72cc1ff3
lande <>
2012-09-07 00:14:22 +0000

    bugfix for multiple souces with default_extra_attrs.

--- python/uw/utilities/xml_parsers.py
+++ python/uw/utilities/xml_parsers.py
@@ -1,7 +1,7 @@
 """Class for parsing and writing gtlike-style sourceEQUATORIAL libraries.
    Barebones implementation; add additional capabilities as users need.
 
-   $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/utilities/xml_parsers.py,v 1.84 2012/08/03 15:08:18 lande Exp $
+   $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/pointlike/python/uw/utilities/xml_parsers.py,v 1.85 2012/08/20 20:14:47 lande Exp $
 
    author: Matthew Kerr
 """
@@ -527,7 +527,6 @@ class Model_to_XML(object):
         self.x2m = XML_to_Model()
         self.debug = debug
         self.strict = strict
-        self.extra_attrs=' '
 
     @staticmethod
     def prepare_model_for_xml(model, scaling, strict):
@@ -578,6 +577,7 @@ class Model_to_XML(object):
         self.pscale = []
         self.perr = []
         self.pfree = []
+        self.extra_attrs = ''
 
         for pointlike_name,gtlike_name in zip(model.param_names, model.gtlike['param_names']):
 
