bfa877dfbd310140d6b725d24adb1e0a81b1470b
kerrm <>
2012-09-13 06:18:52 +0000

    Modify LimitMapper to use tan/arctan transformation.  The sine/arcsine transformation is now in LimitMapperMinuit.

--- python/uw/utilities/parmap.py
+++ python/uw/utilities/parmap.py
@@ -61,6 +61,64 @@ class LogMapper(ParameterMapper):
         return external*LogMapper.log_10
 
 class LimitMapper(ParameterMapper):
+    """ A parameter mapper from the infinite (-inf,+inf) internal
+        parameter space to a finite (a,b) external parameter space.
+
+        The implementation uses the arctangent function.
+
+        Note, scale doesn't acutally do anything, but is included for
+        compatability with gtlike.
+    """
+    def __init__(self, lower, upper, scale=1):
+        self.lower, self.upper = sorted([lower,upper])
+        self.scale = scale
+        self._c = (self.upper-self.lower)/np.pi
+        self._pih = np.pi/2
+
+    def toexternal(self,internal):
+        """ Apply arctan transformation."""
+        return self.lower + self._c*(np.arctan(internal)+self._pih)
+
+    def tointernal(self,external):
+        """ Equation 1.1 of "MINUIT User's Guide" 
+        
+            Make sure mapper protects against bad input:
+
+                >>> mapper = LimitMapper(0, 10)
+                >>> mapper.tointernal(-1)
+                Traceback (most recent call last):
+                    ...
+                MapperException: Parameter value=-1 is smaller than lower limit=0
+                >>> mapper.tointernal(11)
+                Traceback (most recent call last):
+                    ...
+                MapperException: Parameter value=11 is larger than upper limit=10
+        """
+        if external < self.lower:
+            raise MapperException("Parameter value=%s is smaller than lower limit=%s" % (external,self.lower))
+        if external > self.upper:
+            raise MapperException("Parameter value=%s is larger than upper limit=%s" % (external,self.upper))
+
+        return np.tan( (external-self.lower)/self._c-self._pih )
+        
+    def dexternaldinternal(self,external):
+        internal = self.tointernal(external)
+        return self._c/(1.+internal**2)
+
+    def __repr__(self):
+        return 'LimitMapper(%s,%s,%s)' % (self.lower,self.upper,self.scale)
+
+    def __eq__(self, other):
+        """ Previously, equality testing was broken:
+
+                >>> LimitMapper(1,5) == LimitMapper(1,5)
+                True
+                >>> LimitMapper(1,5) == LimitMapper(1,6)
+                False
+        """
+        return self.lower == other.lower and self.upper == other.upper and self.scale == other.scale
+
+class LimitMapperMinuit(ParameterMapper):
     """ Represents a mapper which limits parametesr between lower and upper
 
         See section 1.3.1 of "MINUIT User's Guide":
