360823ecb8626be8db926a1aa0ed54af74d01397
burnett <>
2012-11-24 18:14:25 +0000

    implement argument parsing

--- python/uw/like2/pipeline/create_stream.py
+++ python/uw/like2/pipeline/create_stream.py
@@ -1,20 +1,31 @@
 """
-#Python script to create a stream, using current folder
-"""
-import os, sys
+Python module to create a stream, using current folder
+$Header$
 
-def createStream(stage, sdir):
+"""
+import os, sys, argparse
+
+def createStream(stage, sdir, job_list):
     pipeline='/afs/slac/g/glast/ground/bin/pipeline -m PROD createStream '
-    cmd=pipeline+' -D "stage=%s, SKYMODEL_SUBDIR=%s" UWpipeline' %(stage,sdir)
+    cmd=pipeline+' -D "stage=%s, SKYMODEL_SUBDIR=%s, job_list=%s" UWpipeline' %(stage,sdir,job_list)
     return cmd
 
-assert os.path.exists('config.txt'), 'expect this folder to have a file config.txt'
-cwd = os.getcwd()
-m = cwd.find('skymodels')
-assert m>0, 'did not find "skymodels" in path to cwd, which is %s' %cwd
-sdir = cwd[m:]
-assert len(sys.argv)==2, 'expect a single argument'
-stage = sys.argv[1]
-cmd =createStream(stage, sdir)
-print cmd
-os.system(cmd)
+def main( args ):
+    assert os.path.exists('config.txt'), 'expect this folder to have a file config.txt'
+    cwd = os.getcwd()
+    m = cwd.find('skymodels')
+    assert m>0, 'did not find "skymodels" in path to cwd, which is %s' %cwd
+    sdir = cwd[m:]
+    for stage in args.stage:
+        cmd =createStream(stage, sdir, args.joblist)
+        print cmd
+        if not args.test:
+            os.system(cmd)
+   
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Submit one or more UWpipeline streams; must be in skymodel folder')
+    parser.add_argument('stage', nargs='+', help='the stage indentifier(s)')
+    parser.add_argument('-j','--joblist',  help='Optional list of jobs; assume local to $POINTLIKE_DIR', default='job_list')
+    parser.add_argument('--test', action='store_true', help='Do not run the pipeline createStream')
+    args = parser.parse_args()
+    main(args)
