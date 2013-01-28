94a3f03a91fdb02cd5566a515039c86700188e80
burnett <>
2013-01-28 18:35:03 +0000

    a little more processing info

--- python/uw/data/dataman.py
+++ python/uw/data/dataman.py
@@ -4,8 +4,8 @@ Module implements classes and functions to specify data for use in pointlike ana
 author(s): Matthew Kerr, Eric Wallace
 """
 
-__version__ = '$Revision: 1.20 $'
-#$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/data/dataman.py,v 1.20 2012/11/09 06:29:38 kerrm Exp $
+__version__ = '$Revision: 1.21 $'
+#$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/data/dataman.py,v 1.21 2013/01/28 16:53:21 burnett Exp $
 
 import os, sys
 import collections
@@ -58,7 +58,7 @@ def SimpleCut(vmin,vmax,vuni,colname):
 
     return dssman.make_simple_dss(colname,vuni,vmin,vmax)
 
-def get_default(colname, **kw):
+def get_default(colname, kw):
     """return DSS object for colname
     defaults wired in, except for event class, get info from kw args
     """
@@ -286,7 +286,7 @@ class DataSpec(object):
         for col,mycut in basic_cuts:
             ft1_cut,index = self.dss.get_simple_dss(col)
             if self.__dict__[mycut] is not None:
-                print('Working on {0}'.format(col))
+                print('_make_cuts: working on {0}, cut {1}'.format(col, self.__dict__[mycut] ))
                 if ft1_cut is not None:
                     # apply more restrictive of two cuts
                     self.__dict__[mycut].intersection(ft1_cut)
@@ -353,7 +353,7 @@ class DataSpec(object):
             print('Extracting DSS from existing binfile')
             self.dss = dss
         if self.dss != dss:
-            print 'File %s Failed DSS keyword check: expect %s found %s' % (self.binfile, self.dss, dss)
+            print 'File %s Failed DSS keyword check: expected \n%s \nbut found \n%s' % (self.binfile, self.dss, dss)
             sys.stdout.flush()
             return False
         #
