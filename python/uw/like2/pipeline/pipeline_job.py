a376ee6718f895997f96a1e166c24feb55f2b0b0
burnett <>
2013-03-17 18:33:43 +0000

    implement flexible roi list order

--- python/uw/like2/pipeline/pipeline_job.py
+++ python/uw/like2/pipeline/pipeline_job.py
@@ -1,7 +1,7 @@
 """
 setup and run pointlike all-sky analysis for subset of ROIs
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/pipeline/pipeline_job.py,v 1.9 2012/12/27 15:49:02 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/pipeline/pipeline_job.py,v 1.10 2012/12/29 16:28:07 burnett Exp $
 """
 import os, sys, logging
 from collections import OrderedDict
@@ -21,51 +21,69 @@ defaults=OrderedDict([
 	('PIPELINE_STREAM', '-1'),
     
     ('POINTLIKE_DIR','.'),
-    ('SKYMODEL_SUBDIR','?'),
+    ('SKYMODEL_SUBDIR','.'),
     ('stage', '?'),
     ('begin_roi','2'), # defaults for test: #2 has one source
-    ('end_roi', '3'),
+    ('end_roi', '4'),
+    ('roi_list', '2,'), # comma-delimited list, to replace begin_roi, end_roi if present
     ])
  
-def main( update):
+def main( update=None):
 
     print '\npointlike skymodel configuration'
     for key,default_value in defaults.items():
         item = (key, os.environ.get(key, default_value))
         exec('%s="%s"'%item)
         print '\t%-20s: %s' % item
-    print '--> executing:\n', update.setup()
+    if update is not None:
+        print '--> executing:\n', update.setup()
+        g = update.g()
+    else: 
+        print 'Test execution'
+        def g(n):
+            print 'Would process roi %d' % n
     np.seterr(invalid='warn', divide='warn')
+    
+    if roi_list == '':
+        roi_list = range(int(begin_roi), int(end_roi))
+    else:
+        if roi_list.endswith(','):
+            roi_list=roi_list[:-1]
+        roi_list = map(int, roi_list.split(','))
+    first_roi, last_roi = roi_list[0], roi_list[-1]
+    
+
 
     streamlogdir = os.path.join(POINTLIKE_DIR,SKYMODEL_SUBDIR,'streamlogs')
     streamlogfile=os.path.join(streamlogdir,'stream%s.%04d.log' % ( PIPELINE_STREAMPATH.split('.')[0], int(PIPELINE_STREAM)) )
     if not os.path.exists(streamlogdir): os.mkdir(streamlogdir)
     print 'Logging execution progress to %s' % streamlogfile
     if os.path.exists(streamlogfile):
-       print 'found existing steam log file:%s' % streamlogfile
+       print 'found existing stream log file:%s' % streamlogfile
        with open(streamlogfile) as f:
             lines= f.read().split('\n')
             lastline = lines[-1] if len(lines[-1])>0 else lines[-2]
             if 'Start roi' in lastline: 
-               begin_roi = lastline.split()[-1]
-               print 'Resuming execution with roi at %s' %begin_roi
+               first_roi = lastline.split()[-1]
+               print 'Resuming execution with roi at %s' %first_roi
+               assert first_roi in roi_list, 'Logic error roi % not found in %' %(first_roi, roi_list)
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, filename=streamlogfile   )
 
     ### set up object with skymodel and data
     tzero = tnow = logging.time.time()
-    logging.info('Start setup: rois %s-%s on host %s' % (int(begin_roi), int(end_roi)-1, 
+    logging.info('Start setup: rois %s ... %s on host %s' % (int(first_roi), int(last_roi), 
         os.environ.get('HOSTNAME','unknown')))
 
     mstage = stage
     stage=stage.split(':')[0] # allows multiple stages, separated by colons
         
-    g = update.g()
+    
     tprev, tnow= tnow, logging.time.time()
     logging.info('Finish: elapsed= %.1f (total %.1f)' % ( tnow-tprev, tnow-tzero ))
 
     ### process eash ROI
 
-    for s in range(int(begin_roi), int(end_roi)):
+    for s in roi_list[roi_list.index(first_roi):] :
         logging.info('Start roi %d' % s )
         g(s)
         tprev, tnow= tnow, logging.time.time()
