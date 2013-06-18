7688f6b5746e399679dda29ebb98185b07f75d7e
burnett <>
2013-06-18 19:42:56 +0000

    formatting

--- python/uw/like2/analyze/export.py
+++ python/uw/like2/analyze/export.py
@@ -11,6 +11,7 @@ from .. import to_fits
 import numpy as np
 import pandas as pd
 import pylab as plt
+import pyfits
 
 class Export(dp.SourceInfo):
     """Manage, and document an export step
@@ -28,25 +29,34 @@ class Export(dp.SourceInfo):
         """Log of analysis stream
         <pre>%(logstream)s</pre>"""
         self.startlog()
-        print 'Running to_xml...'
+        print 'Running "to_xml"...'
         to_xml.main()
         
-        print 'Running to_fits...'
+        print '\nRunning "to_fits"...'
         t = to_fits.MakeCat(self.sourcelist)
-        outfile = '_'.join(os.path.abspath('.').split('/')[-2:])+'.fits'
-        t(outfile)
+        self.fits_file = '_'.join(os.path.abspath('.').split('/')[-2:])+'.fits'
+        t(self.fits_file )
         self.logstream=self.stoplog()
          
     def files(self):
         """Links to output files
         <ul>
-         <li>FITS <a href="../../%(fits)s">%(fits)s</a></li>
+         <li>FITS <a href="../../%(fits_file)s">%(fits_file)s</a></li>
          <li>XML  <a href="../../%(xml)s">%(xml)s</ax></li>
         </ul>
         
         """
-        self.fits=glob.glob('*fits')[0]
         self.xml = glob.glob('*.xml')[0]
         
+    def fits_summary(self):
+        """FITS file summary
+        Read back the FITS file, display column information
+        %(fits_summary_table)s
+        """
+        t = pyfits.open(self.fits_file)[1].data
+        df = pd.DataFrame(t)
+        summary = dp.html_table(df.describe().T, float_format=dp.FloatFormat(3), href=False)
+        self.fits_summary_table = summary.replace('%', '%%')
+        
     def all_plots(self):
-        self.runfigures([self.analysis,self.files,])
+        self.runfigures([self.analysis,self.fits_summary, self.files,])
