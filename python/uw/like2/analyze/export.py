6ce782aca69a86173496134355fd4257e7f471f7
burnett <>
2013-06-18 14:27:23 +0000

    use logsstream

--- python/uw/like2/analyze/export.py
+++ python/uw/like2/analyze/export.py
@@ -3,7 +3,7 @@ Export processing
 $Header$
 
 """
-import os
+import os, glob
 
 from uw.like2.pipeline import diagnostic_plots as dp
 from .. import to_xml
@@ -14,13 +14,24 @@ import pylab as plt
 
 class Export(dp.SourceInfo):
     """Manage, and document an export step
+    <p>Expect that findpeak has been run to update the sources file
+    <br>Source files found: %(sourcecsv)s
     """
     def setup(self, **kw):
         super(Export, self).setup(**kw)
-        self.plotdir = 'export'
-        print 'setup Exporting...'
-        
-        pass
-        
+        self.plotfolder = 'export'
+        self.sourcecsv = sorted(glob.glob('source*.csv'))
+        self.sourcelist=pd.read_csv(self.sourcecsv[-1], index_col=0)
+    
+    def log(self):
+        """Log of analysis stream
+        <pre>%(logstream)s</pre>"""
+        self.startlog()
+        to_xml.main()
+        t = to_fits.MakeCat(self.sourcelist)
+        outfile = '_'.join(os.path.abspath('.').split('/')[-2:])+'.fits'
+        t(outfile)
+        self.logstream=self.stoplog()
+         
     def all_plots(self):
-        pass
+        self.runfigures([self.log,])
