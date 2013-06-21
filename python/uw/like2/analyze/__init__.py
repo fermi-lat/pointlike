45d4d43aba2081f1b2315caca63626496d4a7636
burnett <>
2013-06-21 20:33:15 +0000

    new menu scheme

--- python/uw/like2/analyze/__init__.py
+++ python/uw/like2/analyze/__init__.py
@@ -1,4 +1,40 @@
-"""
-Package containing source analysis code
 
 """
+Package containing source analysis code
+$Header$
+
+"""
+__all__=[
+ 'associations',
+# 'components',
+ 'config',
+ 'countplots',
+ 'data',
+ 'environment',
+ 'export',
+ 'find_peak',
+ 'fluxcorr',
+# 'fluxcorriso',
+ 'frontbacksedplots',
+# 'galactic',
+ 'galacticspectra',
+ 'gtlikecomparison',
+ 'hptables',
+# 'isotropic',
+# 'isotropicspectra',
+# 'limb',
+ 'limbrefit',
+ 'localization',
+ 'localization1k',
+# 'pgwseedcheck',
+ 'ptstable',
+ 'pulsarseedcheck',
+ 'roi_info',
+ 'seedcheck',
+ 'sourcecomparison',
+ 'sourceinfo',
+ 'sourcetotal',
+# 'sunmoon',
+ 'sunmoonrefit',
+ 'uwsourcecomparison',
+ ]
