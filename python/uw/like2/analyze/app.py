0d844b4605188c1221b6c7f646a3f2daa60580b4
burnett <>
2013-06-21 20:33:15 +0000

    new menu scheme

--- python/uw/like2/analyze/app.py
+++ python/uw/like2/analyze/app.py
@@ -4,52 +4,44 @@ Application module, allowing command-line access to analysis/plotting tasks
 $Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/analyze/app.py,v 1.5 2013/06/20 23:53:47 burnett Exp $
 
 """
-import os, argparse, types
+_locs = locals().keys()
+from uw.like2.analyze import *
+newlocs = locals().keys()
+module_names = filter(lambda x:not x[0]=='_',set(newlocs).difference(_locs))
+from uw.like2.analyze import html
 import numpy as np
-import pylab as plt
-import pandas as pd
+import sys, types
 
-from uw.like2.analyze import (
-     html, seedcheck, export, sourceinfo, localization, associations,components, countplots, 
-     data, environment, fluxcorriso, fluxcorr, frontbacksedplots, galactic, galacticspectra, 
-     gtlikecomparison, hptables, isotropic, isotropicspectra, limb, limbrefit, pgwseedcheck, 
-     ptstable, pulsarseedcheck, roi_info, sourcecomparison, sourcetotal, sunmoon, sunmoonrefit, 
-     uwsourcecomparison, find_peak
- )
+class AppMenu(dict):
+    
+    def __init__(self, names):
+        super(AppMenu,self).__init__(zip(names, map(self._create_entry, names)))
+        
+    def _create_entry(self, name):
+        pack = globals()[name]
+        doc = pack.__doc__.split('\n')
+        title = (doc[0] if doc[0]!='' else doc[1]).strip()
+        classname = self._check_for_class(pack)
+        if classname is None:
+            print 'no class found in %s' %name
+            return None
+        classobj = eval(name+'.'+classname)
+        return dict(title=title, classname=classname, classobj=classobj, require=getattr(classobj,'require',None),)
 
+    def _check_for_class(self,pack):
+        for name, value in pack.__dict__.items():
+            if name[0]=='_': continue
+            if 'all_plots' in value.__dict__: return name
+        return None
 
-opts = dict(
-        associations=  (associations.Associations,),
-        comparison= (sourcecomparison.SourceComparison,),
-        components= (components.Components,),
-        counts=     (countplots.CountPlots,),
-        data =      (data.Data,),
-        environment=   (environment.Environment,),
-        export=     (export.Export,),
-        fb=         (frontbacksedplots.FrontBackSedPlots,),
-        findpeak=   (find_peak.FindPeak,),
-        fluxcorr=   (fluxcorr.FluxCorr,),
-        fluxcorriso=(fluxcorriso.FluxCorrIso,),
-        galactic=   (galactic.Galactic,),
-        galspect =  (galacticspectra.GalacticSpectra,),
-        gtlikecomparison=(gtlikecomparison.GtlikeComparison,),
-        hptables =  (hptables.HPtables,),
-        isospect =  (isotropicspectra.IsotropicSpectra,),
-        isotropic=  (isotropic.Isotropic,),
-        limb=       (limb.Limb,),
-        limb_refit= (limbrefit.LimbRefit,),
-        localization=(localization.Localization,),
-        pgwseedcheck=(pgwseedcheck.PGWSeedCheck,),
-        pseedcheck= (pulsarseedcheck.PulsarSeedCheck,),
-        pts=        (ptstable.PTStable,),
-        roi=        (roi_info.ROIinfo,),
-        sourceinfo= (sourceinfo.SourceInfo,),
-        sourcetotal=(sourcetotal.SourceTotal,),
-        sunmoon=    (sunmoon.SunMoon,),
-        sunmoon_refit = (sunmoonrefit.SunMoonRefit,),
-        uw_comparison=(uwsourcecomparison.UWsourceComparison,),
-        ) 
+    def __call__(self, name):
+        return self[name]['classobj']()
 
+    def __str__(self):
+        s = '%-15s %s\n' % ('name', 'description')
+        return s+ '\n'.join(['%-15s %s' % (key, self[key]['title']) for key in sorted(self.keys())])  
+
+menu = AppMenu(module_names)
         
         
 def main(args, update_top=False , raise_exception=False):
@@ -57,31 +49,30 @@ def main(args, update_top=False , raise_exception=False):
     success=True
     if type(args)==types.StringType: args = args.split()
     for arg in args:
-        if arg=='all':
-            cs = set(np.hstack(opts.values()))
-            for cls in cs:
-                if os.path.exists(cls.require):
-                    print 'running %s' % cls.__name__
-                    try:
-                        cls('.').all_plots()
-                        plt.close('all')
-                    except Exception, msg:
-                        print '=====failed====\n %s\n=============='% msg
-                else:
-                    print 'skipped %s, missing %s' % (cls.__name__, cls.require)
-            break
+        # ## does not work, needs fixing
+        #if arg=='all':
+        #    cs = set(np.hstack(menu.values()))
+        #    for cls in cs:
+        #        if os.path.exists(cls.require):
+        #            print 'running %s' % cls.__name__
+        #            try:
+        #                cls('.').all_plots()
+        #                plt.close('all')
+        #            except Exception, msg:
+        #                print '=====failed====\n %s\n=============='% msg
+        #        else:
+        #            print 'skipped %s, missing %s' % (cls.__name__, cls.require)
+        #    break
         if arg=='menu': 
             update_top=True
             continue
 
-        if arg not in opts.keys():
-            print 'found %s; expect one of %s' % (arg, opts.keys())
-            continue
+        if arg not in menu.keys():
+            print 'found %s; expect one of %s' % (arg, menu.keys())
             success = False
+            continue
         try:
-            for cls in opts[arg]:
-                cls('.').all_plots()
-                plt.close('all')
+            menu(arg).all_plots()
         except FloatingPointError, msg:
             print 'Floating point error running %s: "%s"' % (arg, msg)
             print 'seterr:', np.seterr()
@@ -97,10 +88,13 @@ def main(args, update_top=False , raise_exception=False):
     return success  
       
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='run an analysis/plotting job; must be in skymodel folder')
-    parser.add_argument('args', nargs='+', help='processsor identifier: must be one of %s' %opts.keys())
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=""" Run an analysis application \n """+menu.__str__()
+    )
+    parser.add_argument('args', nargs='+', help='processsor identifier: must be one of %s' %menu.keys())
     parser.add_argument('--update_top', action='store_true', help='Update the top level Web  menu')
     parser.add_argument('--raise_exception', action='store_true', help ='set to catch exceptions')
     args = parser.parse_args()
     if not main(args.args, update_top=args.update_top, raise_exception=args.raise_exception):
-        raise Exception
+        sys.exit(1)
