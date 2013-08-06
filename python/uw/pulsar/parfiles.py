ba17890619e770a104651eb311686c59de4234c1
kerrm <>
2013-08-06 22:24:13 +0000

    Extra documentation.

--- python/uw/pulsar/parfiles.py
+++ python/uw/pulsar/parfiles.py
@@ -1,7 +1,7 @@
 """
 Module reads and manipulates tempo2 parameter files.
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/pulsar/parfiles.py,v 1.40 2013/08/05 05:44:57 kerrm Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/pulsar/parfiles.py,v 1.41 2013/08/05 06:43:28 kerrm Exp $
 
 author: Matthew Kerr
 """
@@ -536,8 +536,15 @@ class ParFile(dict):
             indicated in block.  Note that we wish to preserve it verbatim
             since it may include comments and errors.  Therefore, delete
             any existing conflicting entries, then prepend this to the
-            file and reload it.
+            file and reload it.  Example of an astrometry block:
+
+            # from Dodson et al. VLBI
+            RAJ    08:35:20.61149  0.00002
+            DECJ   -45:10:34.8751  0.00030
+            
         """
+        if block is None:
+            return
         # remove all duplicate keys
         keys = [l.split()[0] for l in block.split('\n') if l[0] != '#']
         map(self.delete_key,keys)
