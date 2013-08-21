8f275599f7c4d04204152886582cb8ca09853e25
burnett <>
2013-08-21 13:18:59 +0000

    fix docstring

--- python/uw/like2/analyze/sunmoonrefit.py
+++ python/uw/like2/analyze/sunmoonrefit.py
@@ -1,7 +1,7 @@
 """
-Description here
+Sun/moon refit
 
-$Header: /phys/users/glast/python/uw/like2/analyze/sunmoonrefit.py,v 1.144 2013/06/18 12:35:36 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/analyze/sunmoonrefit.py,v 1.1 2013/06/21 20:15:31 burnett Exp $
 
 """
 
@@ -13,6 +13,9 @@ from skymaps import SkyDir
 from . import roi_info
 
 class SunMoonRefit(roi_info.ROIinfo):
+    """ Refit to SunMoon model, showing normalization, change in likelihood
+    %(table)s"""
+
     def setup(self, **kw):
         self.plotfolder = 'sunmoon_refit'
         self.title = 'SunMoon refit'
@@ -46,8 +49,6 @@ class SunMoonRefit(roi_info.ROIinfo):
         self.skyplot_with_hist(self.df.delta_likelihood, 'delta loglike', 0, 5, (0,5))
 
     def all_plots(self):
-        """ Refit to SunMoon model, showing normalization, change in likelihood
-        %(table)s"""
         self.table = pd.DataFrame([self.df.norm, self.df.norm_unc,self.df.delta_likelihood ], 
                 index=['norm', 'norm_unc', 'delta_likelihood']).T.describe().to_html()
         self.runfigures([self.sunmoon_normalization, self.sunmoon_loglikelihood])
