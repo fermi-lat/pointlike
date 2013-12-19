2c32097810e13ac200458be75fa122d9db30e727
burnett <>
2013-12-19 17:43:53 +0000

    add association to finish

--- python/uw/like2/pipeline/uwpipeline.py
+++ python/uw/like2/pipeline/uwpipeline.py
@@ -1,7 +1,7 @@
 """
 task UWpipeline Interface to the ISOC PipelineII
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/pipeline/uwpipeline.py,v 1.35 2013/12/17 14:34:47 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/pipeline/uwpipeline.py,v 1.36 2013/12/18 15:12:16 burnett Exp $
 """
 import os, argparse,  datetime
 import numpy as np
@@ -112,8 +112,8 @@ stagenames = dict(
     update_beta =  StageBatchJob( dict( betafix_flag=True),  sum='sourceinfo',help='check beta', ),
     update_pivot=  StageBatchJob( dict( repivot_flag=True),  sum='sourceinfo',help='update pivot', ), 
     update_only =  StageBatchJob( dict(),                   sum='config counts sourceinfo', help='update, no additional stage', ), 
-    finish      =  StageBatchJob( dict(localize_flag=True,sedfig_dir='sedfig',dampen=0,), 
-                    sum='sourceinfo localization', help='perform localization', ),
+    finish      =  StageBatchJob( dict(localize_flag=True,sedfig_dir='sedfig',dampen=0,associate=True,), 
+                    sum='sourceinfo localization', help='localize, associations, sedfigs', ),
     )
 disabled="""
     tables      =  Stage(pipe.Tables,  sum='hptables', job_list='joblist8.txt', help='create HEALPix tables: ts kde counts', ),
@@ -241,4 +241,3 @@ if __name__=='__main__':
     args = parser.parse_args()
     main(args)
     
- 
