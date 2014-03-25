eccd36bda6988a4410c9e5e88e934a82dce9ceb2
burnett <>
2014-03-25 18:34:03 +0000

    relax tolerance

--- python/uw/like2/main.py
+++ python/uw/like2/main.py
@@ -1,7 +1,7 @@
 """
 Top-level code for ROI analysis
 
-$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/main.py,v 1.70 2014/03/13 21:46:43 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/pointlike/python/uw/like2/main.py,v 1.71 2014/03/24 14:28:37 burnett Exp $
 
 """
 import types, time
@@ -140,7 +140,7 @@ class ROI(views.LikelihoodViews):
                 if set, run the fit in a try block and return None
         update_by : float
             set to zero to not change parameters, or a number between 0 and 1 to make a partial update
-        tolerance : float, default 0.1
+        tolerance : float, default 0.2
             If current fit quality, an estimate of potential improvent of the log likelihood, which is
             based on gradient and hessian is less than this, do not fit
             
@@ -151,7 +151,7 @@ class ROI(views.LikelihoodViews):
         """
         ignore_exception = kwargs.pop('ignore_exception', False)
         update_by = kwargs.pop('update_by', 1.0)
-        tolerance = kwargs.pop('tolerance', 0.1)
+        tolerance = kwargs.pop('tolerance', 0.2)
         
         fit_kw = dict(use_gradient=True, estimate_errors=True)
         fit_kw.update(kwargs)
